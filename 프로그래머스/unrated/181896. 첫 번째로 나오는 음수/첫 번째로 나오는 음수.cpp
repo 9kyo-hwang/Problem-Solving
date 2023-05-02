acc7a07f93eed02083b07975daa8a8cc5d3c89d3
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-05-02 21:51:59 +0900

    [unrated] Title: 첫 번째로 나오는 음수, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.cpp
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.cpp
@@ -4,11 +4,10 @@
 using namespace std;
 
 int solution(vector<int> num_list) {
-    int answer = -1;
     for(int i = 0; i < num_list.size(); i++) {
-        if(num_list[i] < 0 && answer == -1)
-            answer = i;
+        if(num_list[i] < 0)
+            return i;
     }
     
-    return answer;
+    return -1;
 }
