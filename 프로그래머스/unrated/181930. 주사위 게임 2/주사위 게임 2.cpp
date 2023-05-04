75e197b7cbdaf61df81e5529640d80fc845e7b14
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-05-04 15:39:45 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.cpp
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.cpp
@@ -4,10 +4,10 @@
 using namespace std;
 
 int solution(int a, int b, int c) {
-    if(a != b && b != c && a != c)
-        return a + b + c;
-    if((a == b && b != c) || (b == c && a != b) || (a == c && a != b))
-        return (a + b + c) * (a * a + b * b + c * c);
-    if(a == b == c)
-        return (a + b + c) * (a * a + b * b + c * c) * (a * a * a + b * b * b + c * c * c);
+    int answer = a + b + c;
+    if(a == b || b == c || c == a)
+        answer *= (a * a + b * b + c * c);
+    if(a == b && b == c)
+        answer *= (a * a * a + b * b * b + c * c * c);
+    return answer;
 }
