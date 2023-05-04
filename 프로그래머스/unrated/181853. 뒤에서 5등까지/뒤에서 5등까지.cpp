9369a1e1a89a1c58c0703164ec6bbb0f2f25ad40
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-05-04 15:12:23 +0900

    [unrated] Title: 뒤에서 5등까지, Time: 0.02 ms, Memory: 4.01 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    sort(num_list.begin(), num_list.end());
+    return vector<int>(&num_list[0], &num_list[5]);
+}
