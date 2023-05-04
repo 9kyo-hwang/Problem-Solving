6b722caac7fa632eff40376c8ce9e876e3597ede
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-05-04 16:01:58 +0900

    [unrated] Title: 9로 나눈 나머지, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.cpp
+++ 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.cpp
@@ -5,10 +5,7 @@ using namespace std;
 
 int solution(string number) {
     int answer = 0;
-    reverse(number.begin(), number.end());
-    for(const auto &ch : number) {
+    for(const auto &ch : number)
         answer += ch - '0';
-        answer %= 9;
-    }
-    return answer;
+    return answer % 9;
 }
