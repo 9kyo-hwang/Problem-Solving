12f3a23bc1e1e789f723bb6e44016a84e7b68063
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-05-06 19:41:27 +0900

    [unrated] Title: 문자열 뒤집기, Time: 0.01 ms, Memory: 3.64 MB -BaekjoonHub

--- 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.cpp
+++ 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.cpp
@@ -1,15 +1,10 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 string solution(string my_string, int s, int e) {
-    string answer = "";
-    for(int i = 0; i < s; i++)
-        answer.push_back(my_string[i]);
-    for(int i = e; i >= s; i--)
-        answer.push_back(my_string[i]);
-    for(int i = e + 1; i < my_string.length(); i++)
-        answer.push_back(my_string[i]);
-    return answer;
+    reverse(my_string.begin() + s, my_string.begin() + e + 1);
+    return my_string;
 }
