7f3ed7c438faacf80ea3aad1cd3d9f7f1c44aa3e
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-05-07 00:40:41 +0900

    [unrated] Title: 배열의 길이를 2의 거듭제곱으로 만들기, Time: 0.01 ms, Memory: 4.23 MB -BaekjoonHub

--- 프로그래머스/unrated/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.cpp
+++ 프로그래머스/unrated/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 vector<int> solution(vector<int> arr) {
-    while(arr.size() & arr.size() - 1)
+    while(arr.size() & arr.size() - 1) // 2의 제곱수는 비트로 표현 시 왼쪽 1비트만 1이고 나머지 0 -> 따라서 size와 size - 1을 and 연산해서 0이면 size는 2의 거듭제곱수
         arr.emplace_back(0);
     return arr;
 }
