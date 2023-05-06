6e971c22bec17da36273253e7515809e511f3392
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-05-06 21:05:02 +0900

    [unrated] Title: 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기, Time: 0.01 ms, Memory: 3.74 MB -BaekjoonHub

--- 프로그래머스/unrated/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.cpp
+++ 프로그래머스/unrated/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.cpp
@@ -5,10 +5,5 @@
 using namespace std;
 
 string solution(string myString, string pat) {
-    auto pos = 0;
-    while(myString.find(pat, pos) != string::npos) {
-        pos = myString.find(pat, pos) + pat.length();
-    }
-    string answer = myString.substr(0, pos);
-    return answer;
+    return myString.substr(0, myString.rfind(pat) + pat.length());
 }
