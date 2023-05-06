95a49aac88484ccfbccba3fc66130c6d0fc2340f
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-05-07 01:12:46 +0900

    [unrated] Title: 정사각형으로 만들기, Time: 2.26 ms, Memory: 6.37 MB -BaekjoonHub

--- 프로그래머스/unrated/181830. 정사각형으로 만들기/정사각형으로 만들기.cpp
+++ 프로그래머스/unrated/181830. 정사각형으로 만들기/정사각형으로 만들기.cpp
@@ -1,17 +1,14 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 vector<vector<int>> solution(vector<vector<int>> arr) {
-    int sizeof2d = arr.size(), sizeof1d = arr[0].size();
-    if(sizeof2d > sizeof1d) { // 행 > 열
-        for(auto& v : arr)
-            for(int i = 0; i < sizeof2d - sizeof1d; i++)
-                v.emplace_back(0);
-    } else if(sizeof1d > sizeof2d) { // 열 > 행
-        for(int i = 0; i < sizeof1d - sizeof2d; i++)
-            arr.emplace_back(vector<int>(sizeof1d, 0));
-    }
+    int row = arr.size(), col = arr[0].size();
+    int size = max(row, col);
+    for(auto& v : arr)
+        v.resize(size, 0); // 원본 크기가 size보다 작다면 수행되지 않음
+    arr.resize(size, vector<int>(size, 0));
     return arr;
 }
