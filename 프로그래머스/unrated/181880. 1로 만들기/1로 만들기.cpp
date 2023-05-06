fcd44311b64e487abb3c17749d720de4fd6db341
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-05-06 19:49:26 +0900

    [unrated] Title: 1로 만들기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.cpp
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.cpp
@@ -7,7 +7,7 @@ int solution(vector<int> num_list) {
     int answer = 0;
     for(auto &num : num_list) {
         while(num > 1) {
-            num = (num % 2 == 0 ? num / 2 : (num - 1) / 2);
+            num /= 2; // 홀수여도 어차피 내림 연산이 적용됨.
             answer++;
         }
     }
