f179f04ac6a6f4de93a6099bebd6eef70a5117f0
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-05-08 01:27:52 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 0.28 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.cpp
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.cpp
@@ -5,14 +5,10 @@
 using namespace std;
 
 vector<int> solution(string s) {
-    vector<int> answer(2);
-    
     int turn = 0;
     int dist = 0;
-    while(true) {
-        if(s == "1")
-            break;
-        
+    
+    while(s != "1") {
         auto it = remove(s.begin(), s.end(), '0');
         dist += distance(it, s.end());
         s.erase(it, s.end());
@@ -24,9 +20,9 @@ vector<int> solution(string s) {
             len /= 2;
         }
         s = binary;
+        
         turn++;
     }
     
-    answer[0] = turn, answer[1] = dist;
-    return answer;
+    return {turn, dist};
 }
