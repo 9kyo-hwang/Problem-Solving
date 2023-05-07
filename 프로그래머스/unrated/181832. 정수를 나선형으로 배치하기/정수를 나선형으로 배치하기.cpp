301e37b3f0676a6aff07770654e72b7f7d08f728
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-05-07 22:35:58 +0900

    [unrated] Title: 정수를 나선형으로 배치하기, Time: 0.19 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.cpp
+++ 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.cpp
@@ -3,31 +3,38 @@
 
 using namespace std;
 
+int OFFSET[][2] = {
+    {0, 1}, // E
+    {1, 0}, // S
+    {0, -1}, // W
+    {-1, 0} // N
+};
+
 vector<vector<int>> solution(int n) {
     vector<vector<int>> arr(n, vector<int>(n, 0));
     int i = 0, j = 0;
     int num = 1;
     
     while(num <= n * n) {
-        while(j < n && arr[i][j] == 0)
-            arr[i][j++] = num++;
-        j--;
-        i++;
-        
-        while(i < n && arr[i][j] == 0) 
-            arr[i++][j] = num++;
-        i--;
-        j--;
-        
-        while(j >= 0 && arr[i][j] == 0) 
-            arr[i][j--] = num++;
-        j++;
-        i--;
-        
-        while(i >= 0 && arr[i][j] == 0)
-            arr[i--][j] = num++;
-        i++;
-        j++;
+        for(int dir = 0; dir < 4; dir++) {
+            while(0 <= i && i < n && 0 <= j && j < n && arr[i][j] == 0) {
+                arr[i][j] = num++;
+                
+                switch(dir) {
+                    case 0: j++; break;
+                    case 1: i++; break;
+                    case 2: j--; break;
+                    case 3: i--; break;
+                }
+            }
+            
+            switch(dir) {
+                case 0: j--, i++; break;
+                case 1: i--, j--; break;
+                case 2: j++, i--; break;
+                case 3: i++, j++; break;
+            }
+        }
     }
     
     return arr;
