fbf0d7d88cd3096e30aced2dc73d506fec013141
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-05-08 13:46:32 +0900

    [level 2] Title: 피보나치 수, Time: 0.53 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.cpp
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 
 int solution(int n) {
-    vector<long long> fib(100001, 0);
+    vector<int> fib(n + 1, 0);
     fib[0] = 0, fib[1] = 1;
-    for(int i = 2; i <= 100000; i++)
+    for(int i = 2; i <= n; i++)
         fib[i] = (fib[i - 2] + fib[i - 1]) % 1234567;
     
     return fib[n];
