7ab78420c4c0c6705ced9484637ad29a0c2bd3ce
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-05-15 16:25:40 +0900

    [Bronze III] Title: 사격 내기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/27960. 사격 내기/사격 내기.cc
+++ 백준/Bronze/27960. 사격 내기/사격 내기.cc
@@ -1,43 +1,8 @@
-#include <iostream>
-#include <vector>
+#include <stdio.h>
 
-using namespace std;
-
-int main()
-{
+int main() {
     int a, b;
-    cin >> a >> b;
-    
-    vector<bool> setted[2];
-    setted[0] = vector<bool>(10, false);
-    setted[1] = vector<bool>(10, false);
-    
-    int score = 512;
-    int index = 9;
-    while(score) {
-        if(a >= score) {
-            a -= score;
-            setted[0][index] = true;
-        }
-        if(b >= score) {
-            b -= score;
-            setted[1][index] = true;
-        }
-        
-        score /= 2;
-        --index;
-    }
-    
-    int c = 0;
-    score = 1;
-    for(int i = 0; i < 10; i++) {
-        if(setted[0][i] && !setted[1][i] || !setted[0][i] && setted[1][i])
-            c += score;
-        
-        score *= 2;
-    }
-    
-    cout << c;
-    
+    scanf("%d %d", &a, &b);
+    printf("%d", a^b);
     return 0;
 }
