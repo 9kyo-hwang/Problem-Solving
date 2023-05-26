ce29c05ae3fb927651ba66fe722edd440f794852
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-05-26 22:42:41 +0900

    [Silver II] Title: 부분수열의 합, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.cc
@@ -3,30 +3,13 @@
 
 using namespace std;
 
-int N, S, count = 0;
+int N, S;
 vector<int> num;
-vector<bool> include;
 
-void PowerSet(const int& max_select, int cur_select = 0, int index = 0) {
-  if(cur_select == max_select) {
-    int cur_sum = 0;
-    for(int i = 0; i <= index; i++)
-      if(include[i])
-        cur_sum += num[i];
-
-    if(cur_sum == S)
-      count++;
-
-    return;
-  }
-
+int PowerSet(int index = 0, int sum = 0) {
   if(index >= N)
-    return;
-
-  include[index] = true;
-  PowerSet(max_select, cur_select + 1, index + 1);
-  include[index] = false;
-  PowerSet(max_select, cur_select, index + 1);
+    return sum == S;
+  return PowerSet(index + 1, sum + num[index]) + PowerSet(index + 1, sum);
 }
 
 int main() {
@@ -38,12 +21,10 @@ int main() {
   num = vector<int>(N);
   for(auto &i : num)
     cin >> i;
-  include = vector<bool>(N, false);
 
-  for(int k = 1; k <= N; k++) {
-    PowerSet(k);
-  }
-
+  int count = PowerSet();
+  if(S == 0)
+    --count;
   cout << count;
 
   return 0;
