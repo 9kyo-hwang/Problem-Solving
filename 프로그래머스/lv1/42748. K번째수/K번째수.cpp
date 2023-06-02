ea32d8990471fe608ad5cdfa6cec7592ded3ca31
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-06-03 02:36:04 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    for(const auto& command : commands) {
+        int i = command[0];
+        int j = command[1];
+        int k = command[2];
+        
+        auto tmp = vector<int>(&array[i - 1], &array[j]);
+        sort(tmp.begin(), tmp.end());
+        answer.emplace_back(tmp[k - 1]);
+    }
+    return answer;
+}
