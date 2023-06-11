4b5a28dc0d66265d17fa83c52df036590c003bc6
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-06-11 22:56:05 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.cpp
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.cpp
@@ -1,26 +1,22 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 int solution(vector<int> ingredient) {
-    string pattern = "1231";
-    string str;
-    for(const auto& i : ingredient)
-        str += char(i + '0');
-    
     int answer = 0;
-    int pos = 0;
-    while(true) {
-        const auto p = str.find(pattern, pos);
-        if (p == std::string::npos)
-            break;
-        
-        str.erase(p, 4);
-        if (p > 2)
-            pos = p - 3;
-        answer++;
-    }
+    vector<int> stack = { -1 };
+    for(const auto &i : ingredient){
+        if(stack.back() == 1 && i == 2) {
+            stack.back() = 12;
+        } else if(stack.back() == 12 && i == 3) {
+            stack.back() = 123;
+        } else if(stack.back() == 123 && i == 1) {
+            answer++;
+            stack.pop_back();
+        } else {
+            stack.emplace_back(i);
+        }
+    }    
     return answer;
 }
