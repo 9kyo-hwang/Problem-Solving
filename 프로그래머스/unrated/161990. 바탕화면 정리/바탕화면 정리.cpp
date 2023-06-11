53113cc18d2423a7512d970b05b67753f2c2e2d9
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-06-11 19:33:18 +0900

    [unrated] Title: 바탕화면 정리, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<string> wallpaper) {
+    int lux = 51, luy = 51, rdx = -1, rdy = -1;
+    for(int i = 0; i < wallpaper.size(); i++) {
+        for(int j = 0; j < wallpaper[i].length(); j++) {
+            if(wallpaper[i][j] == '#') {
+                lux = min(lux, i);
+                luy = min(luy, j);
+                rdx = max(rdx, i);
+                rdy = max(rdy, j);
+            }
+        }
+    }
+    return {lux, luy, rdx + 1, rdy + 1};
+}
