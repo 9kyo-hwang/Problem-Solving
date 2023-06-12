66c45e1f3ac24420ef444638ae2377e854f28804
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-06-12 22:49:00 +0900

    [unrated] Title: 옹알이 (2), Time: 0.11 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.cpp
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.cpp
@@ -1,30 +1,20 @@
 #include <string>
 #include <vector>
-#include <unordered_set>
+#include <regex>
 
 using namespace std;
 
-vector<string> words = {"aya", "ye", "woo", "ma"};
-unordered_set<string> set;
-
-void Combinations(const string &current) {
-    if(current.length() > 30)
-        return;
-    
-    if(!current.empty())
-        set.emplace(current);
-    
-    for(const auto &word: words) {
-        if(current.empty() || current.substr(current.length() - 2) != word.substr(word.length() - 2))
-            Combinations(current + word);
-    }
-}
-
 int solution(vector<string> babbling) {
     int answer = 0;
-    Combinations("");
-    for(const auto &str : babbling) {
-        if(set.find(str) != set.end())
+    regex duplicate("(ayaaya|yeye|woowoo|mama)");
+    regex pattern("(aya|ye|woo|ma)");
+    for(auto &str : babbling) {
+        if(regex_search(str, duplicate))
+            continue;
+        
+        str = regex_replace(str, pattern, "");
+        
+        if(str.empty())
             answer += 1;
     }
     return answer;
