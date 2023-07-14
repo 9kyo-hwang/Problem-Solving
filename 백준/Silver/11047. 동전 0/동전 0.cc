39ea4146e8810fa40c01104ea7d6bd344dc9b2ea
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-07-14 18:10:26 +0900

    [Silver IV] Title: 동전 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.cc
+++ 백준/Silver/11047. 동전 0/동전 0.cc
@@ -3,25 +3,26 @@
 
 using namespace std;
 
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr); cout.tie(nullptr);
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
 
-	int N, K; cin >> N >> K;
-	vector<int> coin(N);
+  int N, K;
+  cin >> N >> K;
 
-	for (auto& i : coin) cin >> i;
+  vector<int> coin_types(N, 0);
+  for(int i = N - 1; i >= 0; i -= 1) {
+    cin >> coin_types[i];
+  }
 
-	int count = 0;
-	for(auto it = coin.rbegin(); it != coin.rend(); ++it)
-	{
-		count += K / *it; // 나눈 몫만큼 개수 증가
-		K %= *it; // K를 몫으로 나눈 나머지로 갱신
-		// K < *it인 경우 몫이 0이고 나머지도 원본 값 그대로 유지됨
-	}
+  int count = 0;
+  for(const auto &coin : coin_types) {
+    count += K / coin;
+    K %= coin;
+  }
 
-	cout << count << "\n";
+  cout << count;
 
-	return 0;
+  return 0;
 }
