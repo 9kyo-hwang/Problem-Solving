082742e3df4fc571c954eb5eab71fb4f4a5757e6
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-07-15 22:39:10 +0900

    [Silver II] Title: 제곱수의 합, Time: 32 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.cc
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.cc
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <cmath>
 
 using namespace std;
 
@@ -13,17 +12,12 @@ int main() {
   int N;
   cin >> N;
 
-  vector<int> dp(N + 1);
+  vector<int> dp(N + 1, INT32_MAX);
   dp[0] = 0;
 
-  for(int i = 1; i <= N; i += 1) {
-    if(sqrt(i) == (int)sqrt(i)) { // 제곱수면 1
-      dp[i] = 1;
-    } else {
-      dp[i] = i;
-      for(int j = 1; j * j <= i; j += 1) { // 아니라면 i보다 작은 제곱수 + (i - 제곱수)의 경우의 수 합
-        dp[i] = min(dp[i], dp[j * j] + dp[i - j * j]);
-      }
+  for (int i = 1; i <= N; i += 1) {
+    for (int j = 1; j * j <= i; j += 1) {
+      dp[i] = min(dp[i], 1 + dp[i - j * j]);
     }
   }
 
