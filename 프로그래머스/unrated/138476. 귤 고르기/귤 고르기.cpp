678e4e7205504b8806d732525a6d2774246cae94
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-07-23 20:03:00 +0900

    [unrated] Title: 귤 고르기, Time: 797.39 ms, Memory: 273 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.cpp
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.cpp
@@ -1,27 +1,26 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
-int solution(int k, vector<int> tangerine) {
-    vector<int> counts(10000001, 0);
+int solution(int k, vector<int> tangerine) {    
+    unordered_map<int, int> counts;
     for(const int &i : tangerine) {
         counts[i] += 1;
     }
     
     vector<pair<int, int>> v;
-    for(int i = 1; i <= 10000000; i += 1) {
-        if(counts[i] != 0) {
-            v.emplace_back(counts[i], i);
-        }
+    for(int i = 0; i < counts.size(); i += 1) {
+        v.emplace_back(counts[i], i + 1);
     }
     
     sort(v.begin(), v.end());
     
     int size = v.size(); // num of size type
     int i;
-    for(i = size -1 ; i >= 0; i -= 1) {
+    for(i = size - 1; i >= 0; i -= 1) {
         const auto &[cnt, idx] = v[i];
         k -= cnt;
         
