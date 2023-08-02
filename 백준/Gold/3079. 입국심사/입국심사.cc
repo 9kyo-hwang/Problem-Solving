67199b3316fd0b18c27a35390a772a23d2a39468
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-08-03 01:48:53 +0900

    [Gold V] Title: 입국심사, Time: 72 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Gold/3079. 입국심사/입국심사.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+using ull = unsigned long long;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int N, M;
+  cin >> N >> M;
+  
+  vector<ull> times(N);
+  for(ull &i : times) {
+    cin >> i;
+  }
+  sort(times.begin(), times.end());
+
+  // 가장 빨리 끝나는 경우: 1분 걸리는 심사관한테 1명
+  // 가장 늦게 끝나는 경우: 가장 오래 걸리는 심사관한테 M명
+  ull min_time = 1, max_time = times.back() * M;
+  ull answer = 0;
+  while(min_time <= max_time) {
+    ull avg_time = (min_time + max_time) / 2;
+    ull pass = 0;
+
+    // 지금 평균 처리 시간으로 몇 명을 심사할 수 있는가?
+    for(const ull &t : times) {
+      pass += avg_time / t;
+    }
+
+    if(pass >= M) { // 모두 처리할 수 있다면 최대 시간 감소
+      max_time = avg_time - 1;
+      answer = avg_time;
+    } else { // 불가능 하다면 최소 시간 증가
+      min_time = avg_time + 1;
+    }
+  }
+
+  cout << answer;
+
+  return 0;
+}
