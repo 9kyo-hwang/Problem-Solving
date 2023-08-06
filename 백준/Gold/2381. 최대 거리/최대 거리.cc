1a3692d2a29ac66d4a4f2d6432ec54bba9f6226a
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-08-06 22:50:16 +0900

    [Gold III] Title: 최대 거리, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2381. 최대 거리/최대 거리.cc
+++ 백준/Gold/2381. 최대 거리/최대 거리.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 
 using namespace std;
@@ -12,13 +11,11 @@ int main() {
   int N;
   cin >> N;
 
-  vector<pair<int, int>> v(N);
-  for (auto &[x, y] : v) {
-    cin >> x >> y;
-  }
-
+  int x, y;
   int max_sum = -2000000, min_sum = 2000000, max_diff = -2000000, min_diff = 2000000;
-  for (const auto &[x, y] : v) {
+  while(N--) {
+    cin >> x >> y;
+    
     max_sum = max(max_sum, x + y);
     min_sum = min(min_sum, x + y);
 
