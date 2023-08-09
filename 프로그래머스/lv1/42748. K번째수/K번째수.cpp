2200a81ff912c805b330821fc0f387c051c1f5d4
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-08-09 18:46:41 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.cpp
+++ 프로그래머스/lv1/42748. K번째수/K번째수.cpp
@@ -4,16 +4,30 @@
 
 using namespace std;
 
+int QuickSelect(const vector<int> &array, int k) {
+  if (array.size() <= 1)
+    return array[0];
+
+  int pivot = array[array.size() / 2];
+  vector<int> left, mid, right;
+  copy_if(array.begin(), array.end(), back_inserter(left), [&](int x) { return x < pivot; });
+  copy_if(array.begin(), array.end(), back_inserter(mid), [&](int x) { return x == pivot; });
+  copy_if(array.begin(), array.end(), back_inserter(right), [&](int x) { return x > pivot; });
+
+  if (k < left.size()) {
+    return QuickSelect(left, k);
+  } else if (k < left.size() + mid.size()) {
+    return mid[0];
+  } else {
+    return QuickSelect(right, k - (left.size() + mid.size()));
+  }
+}
+
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
-    vector<int> answer;
-    for(const auto& command : commands) {
-        int i = command[0];
-        int j = command[1];
-        int k = command[2];
-        
-        vector<int> tmp(&array[i - 1], &array[j]);
-        sort(tmp.begin(), tmp.end());
-        answer.emplace_back(tmp[k - 1]);
-    }
-    return answer;
+  vector<int> answer;
+  for (const auto &command : commands) {
+    int i = command[0], j = command[1], k = command[2];
+    answer.emplace_back(QuickSelect(vector<int>(&array[i - 1], &array[j]), k - 1));
+  }
+  return answer;
 }
