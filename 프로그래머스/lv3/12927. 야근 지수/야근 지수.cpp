9e49f203a2dc3902719e5d1c648558cff7f2b5c7
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-08-12 11:51:34 +0900

    [level 3] Title: 야근 지수, Time: 18.08 ms, Memory: 3.7 MB -BaekjoonHub

--- 프로그래머스/lv3/12927. 야근 지수/야근 지수.cpp
+++ 프로그래머스/lv3/12927. 야근 지수/야근 지수.cpp
@@ -6,13 +6,13 @@ using namespace std;
 
 long long solution(int n, vector<int> works) {
     priority_queue<int> pq(works.begin(), works.end());
-    
+
     while(pq.top() > 0 && n--) {
         int top = pq.top();
         pq.pop();
         pq.emplace(top - 1);
     }
-    
+
     long long answer = 0;
     while(!pq.empty()) {
         int top = pq.top();
