1eaadde0855e2b591eedd9de256cda207fd77aaa
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-08-19 01:08:58 +0900

    [Silver III] Title: 1로 만들기, Time: 0 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,43 +1,28 @@
 #include <iostream>
-#include <queue>
+#include <vector>
+#include <algorithm>
+
+#define FASTIO ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define INF 1e9
 
 using namespace std;
 
+vector<int> dp;
+int solution(int x) {
+    if(x <= 1)
+        return 0;
+    else if(dp[x] > 0)
+        return dp[x];
+    else
+        return dp[x] = min({solution(x / 3) + x % 3 + 1, solution(x / 2) + x % 2 + 1});
+}
+
 int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
-  cout.tie(nullptr);
-
-  int n;
-  cin >> n;
-
-  vector visited(n, 0);
-  queue<pair<int, int>> q;
-  q.emplace(n, 0);
-
-  while (!q.empty()) {
-    auto [num, step] = q.front();
-    q.pop();
-
-    if (num == 1) {
-      cout << step;
-      break;
-    }
-
-    if (num % 3 == 0 && visited[num / 3] == 0) {
-      visited[num / 3] = 1;
-      q.emplace(num / 3, step + 1);
-    }
-
-    if (num % 2 == 0 && visited[num / 2] == 0) {
-      visited[num / 2] = 1;
-      q.emplace(num / 2, step + 1);
-    }
-
-    if (visited[num - 1] == 0) {
-      visited[num - 1] = 1;
-      q.emplace(num - 1, step + 1);
-    }
-  }
-  return 0;
+    FASTIO
+        
+    int X; cin >> X;
+    dp = vector<int>(X + 1, 0);
+    cout << solution(X);
+    
+    return 0;
 }
