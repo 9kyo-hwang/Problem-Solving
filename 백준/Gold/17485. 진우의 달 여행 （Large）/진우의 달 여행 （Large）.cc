586c79538263e327b50d0b89cb07a343f5f355f3
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-08-21 03:55:46 +0900

    [Gold V] Title: 진우의 달 여행 (Large), Time: 92 ms, Memory: 21840 KB -BaekjoonHub

--- 백준/Gold/17485. 진우의 달 여행 （Large）/진우의 달 여행 （Large）.cc
+++ 백준/Gold/17485. 진우의 달 여행 （Large）/진우의 달 여행 （Large）.cc
@@ -5,50 +5,45 @@
 using namespace std;
 
 constexpr int MAX = 100001;
-constexpr int OFFSET[] = {-1, 0, 1,};
-
-int N, M;
-vector<vector<int>> m;
-vector<vector<vector<int>>> L;
-
-int mat(int i, int j, int pd) {
-  if (L[pd][i][j] != MAX) return L[pd][i][j];
-
-  if (i == 0) { // 시작선
-    L[pd][i][j] = m[i][j];
-  } else {
-    for (const int dir : {0, 1, 2}) {
-      if (dir == pd) continue;
-
-      int nj = j + OFFSET[dir];
-      if (nj < 0 || nj >= M) continue;
-
-      L[pd][i][j] = min(L[pd][i][j], mat(i - 1, nj, dir) + m[i][j]);
-    }
-  }
-
-  return L[pd][i][j];
-}
 
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
   cout.tie(nullptr);
 
+  int N, M;
   cin >> N >> M;
 
-  m = vector<vector<int>>(N, vector<int>(M));
-  for (auto &r : m) {
-    for (auto &c : r) {
-      cin >> c;
-    }
-  }
-  L = vector<vector<vector<int>>>(3, vector<vector<int>>(N, vector<int>(M, MAX)));
-
+  vector<vector<int>> costs(N, vector<int>(M));
+  vector<vector<vector<int>>> dp(3, vector<vector<int>>(N, vector<int>(M, MAX))); // d 방향으로 [i, j] 칸을 진입함
   int answer = MAX;
-  for (int j = 0; j < M; j++) {
-    for(const int dir : {0, 1, 2}) {
-      answer = min(answer, mat(N - 1, j, dir));
+  
+  for(int i = 0; i < N; i++) {
+    for(int j = 0; j < M; j++) {
+      cin >> costs[i][j];
+      
+      if(i == 0) {
+        for(int d = 0; d < 3; d++) {
+          dp[d][i][j] = costs[i][j];
+        }
+        continue;
+      }
+
+      if(j != 0) { // 왼쪽 벽이 아니라면 좌상단 진입 확인
+        dp[0][i][j] = min(dp[1][i - 1][j - 1], dp[2][i - 1][j - 1]) + costs[i][j];
+      }
+
+      if(j != M - 1) { // 오른쪽 벽이 아니라면 우상단 진입 확인
+        dp[2][i][j] = min(dp[0][i - 1][j + 1], dp[1][i - 1][j + 1]) + costs[i][j];
+      }
+
+      dp[1][i][j] = min(dp[0][i - 1][j], dp[2][i - 1][j]) + costs[i][j]; // 모든 위치에서 상단 진입 확인
+
+      if(i == N - 1) {
+        for(int d = 0; d <= 2; d++) {
+          answer = min(answer, dp[d][i][j]);
+        }
+      }
     }
   }
 
