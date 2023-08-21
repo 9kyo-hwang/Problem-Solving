f827a9e1a8b7e73f43af37e94fe96ea60bf36b7a
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-08-22 00:53:26 +0900

    [level 3] Title: 등굣길, Time: 0.05 ms, Memory: 3.66 MB -BaekjoonHub

--- 프로그래머스/lv3/42898. 등굣길/등굣길.cpp
+++ 프로그래머스/lv3/42898. 등굣길/등굣길.cpp
@@ -11,18 +11,13 @@ int solution(int m, int n, vector<vector<int>> puddles) {
     for(const auto p : puddles) // 갈 수 없는 위치
         dp[p[1]][p[0]] = MAX;
     
+    dp[1][1] = 1;
     for(int i = 1; i <= n; i++) {
         for(int j = 1; j <= m; j++) {
             if(dp[i][j] == MAX) { // 갈 수 없는 위치는 이 좌표에 도달할 수 있는 경우의 수를 0으로 설정
                 dp[i][j] = 0;
-            } else if(i == 1 && j == 1) { // 시작점
-                dp[i][j] = 1;
-            } else if(i == 1) { // 1행은 왼쪽에서만 올 수 있음
-                dp[i][j] = dp[i][j - 1];
-            } else if(j == 1) { // 1열은 위쪽에서만 올 수 있음
-                dp[i][j] = dp[i - 1][j];
             } else {
-                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % MAX;
+                dp[i][j] += (dp[i - 1][j] + dp[i][j - 1]) % MAX;
             }
         }
     }
