d55b2736dfc700a4164c37982912cdf5aed5faed
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-08-25 00:32:18 +0900

    [level 3] Title: 합승 택시 요금, Time: 0.66 ms, Memory: 3.81 MB -BaekjoonHub

--- 프로그래머스/lv3/72413. 합승 택시 요금/합승 택시 요금.cpp
+++ 프로그래머스/lv3/72413. 합승 택시 요금/합승 택시 요금.cpp
@@ -4,15 +4,13 @@
 
 using namespace std;
 
-constexpr int INF = 1e9;
+constexpr int INF = 1e8;
 
 int solution(int n, int s, int a, int b, vector<vector<int>> fares) {
     vector graph(n + 1, vector(n + 1, INF));
     for(const auto& fare : fares) {
         int c = fare[0], d = fare[1], f = fare[2];
-        
-        graph[c][d] = f;
-        graph[d][c] = f;
+        graph[c][d] = graph[d][c] = f;
     }
     
     for(int i = 1; i <= n; i++)
@@ -28,8 +26,6 @@ int solution(int n, int s, int a, int b, vector<vector<int>> fares) {
     
     int answer = INF;
     for(int k = 1; k <= n; k++) {
-        if(graph[s][k] == INF || graph[k][a] == INF || graph[k][b] == INF)
-            continue;
         answer = min(answer, graph[s][k] + graph[k][a] + graph[k][b]);
     }
     return answer;
