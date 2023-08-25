aeae850c2ad71c1fe73363e6022eb0e078ba061d
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-08-26 01:56:58 +0900

    [level 2] Title: 카카오프렌즈 컬러링북, Time: 2.87 ms, Memory: 5.34 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/1829. 카카오프렌즈 컬러링북/카카오프렌즈 컬러링북.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+
+using namespace std;
+
+vector<vector<bool>> visited;
+int count_cells(vector<vector<int>> &p, const int m, const int n, const int c, int x, int y) {
+    if(x < 0 || x >= m || y < 0 || y >= n || visited[x][y]) {
+        return 0;
+    } else if(p[x][y] == 0 || p[x][y] != c) {
+        return 0;
+    } else {
+        visited[x][y] = true;
+        return 1 + count_cells(p, m, n, c, x - 1, y) + count_cells(p, m, n, c, x, y + 1) + count_cells(p, m, n, c, x + 1, y) + count_cells(p, m, n, c, x, y - 1);
+    }
+}
+
+vector<int> solution(int m, int n, vector<vector<int>> picture) {
+    visited = vector(m, vector(n, false));
+    int cnt = 0;
+    int max_cells = 0;
+    for(int x = 0; x < m; x++) {
+        for(int y = 0; y < n; y++) {
+            if(picture[x][y] == 0 || visited[x][y])
+                continue;
+            
+            int c = picture[x][y];
+            cnt += 1;
+            
+            int result = count_cells(picture, m, n, c, x, y);
+            if(result > max_cells) {
+                max_cells = result;
+            }
+        }
+    }
+    
+    return {cnt, max_cells};
+}
