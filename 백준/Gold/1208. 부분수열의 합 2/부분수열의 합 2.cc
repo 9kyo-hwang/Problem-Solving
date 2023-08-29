f5799e819f46962dd7c1e6a67249d9e7457372ca
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-08-29 22:55:04 +0900

    [Gold I] Title: 부분수열의 합 2, Time: 280 ms, Memory: 41324 KB -BaekjoonHub

--- 백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.cc
+++ 백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.cc
@@ -1,38 +1,39 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
-#include <functional>
 
 using namespace std;
 using ll = long long;
+
+int N, S;
+vector<int> v;
+unordered_map<int, int> map;
+long long cnt = 0;
+
+void powerset(int index, int sum, bool flag) {
+	if (flag && index == N / 2) {
+		map[sum]++;
+		return;
+	}
+	else if (!flag && index == N) {
+		cnt += map[S - sum];
+		return;
+	}
+
+	powerset(index + 1, sum, flag);
+	powerset(index + 1, sum + v[index], flag);
+}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 
-	int N, S;
 	cin >> N >> S;
 
-	vector<int> v(N);
+	v.resize(N);
 	for (int& i : v)
 		cin >> i;
-
-	unordered_map<int, int> map;
-	long long cnt = 0;
-	function<void(int, int, bool)> powerset = [&](int index, int sum, bool flag) {
-		if (flag && index == N / 2) {
-			map[sum]++;
-			return;
-		}
-		else if (!flag && index == N) {
-			cnt += map[S - sum];
-			return;
-		}
-
-		powerset(index + 1, sum, flag);
-		powerset(index + 1, sum + v[index], flag);
-		};
 
 	powerset(0, 0, true);
 	powerset(N / 2, 0, false);
