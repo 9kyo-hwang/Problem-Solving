bb51d6c0013b97d4fc690accdd3c3d0694c4f913
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-09-04 20:02:33 +0900

    [Gold III] Title: ACM Craft, Time: 152 ms, Memory: 2832 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.cc
+++ 백준/Gold/1005. ACM Craft/ACM Craft.cc
@@ -11,27 +11,33 @@ int main()
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 
-	int T; cin >> T;
+	int T, N, K, W, X, Y; 
+	vector<int> build_times;
+	vector<vector<int>> graph;
+	vector<int> in_degress, dp;
+
+	cin >> T;
 	while (T--)
 	{
-		int N, K; cin >> N >> K;
+		cin >> N >> K;
 
-		vector<int> build_times(N + 1, 0);
-		for (int i = 1; i <= N; i++) cin >> build_times[i];
+		build_times.assign(N + 1, 0);
+		for (int i = 1; i <= N; i++) 
+			cin >> build_times[i];
 
-		vector<vector<int>> graph(N + 1);
-		vector<int> in_degress(N + 1, 0);
+		graph.assign(N + 1, {});
+		in_degress.assign(N + 1, 0);
 		while (K--)
 		{
-			int X, Y; cin >> X >> Y;
+			cin >> X >> Y;
 			graph[X].emplace_back(Y);
 			in_degress[Y] += 1;
 		}
 
-		int W; cin >> W;
+		cin >> W;
 
 		queue<int> q;
-		vector<int> dp(N + 1, 0);
+		dp.assign(N + 1, 0);
 		for (int i = 1; i <= N; i++)
 		{
 			if (in_degress[i] == 0)
