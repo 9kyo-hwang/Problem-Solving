6ef7798c272ec3fb0a6522e5a0fc24b7e426f6dd
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-09-09 22:33:58 +0900

    [level 2] Title: [3차] 압축, Time: 0.17 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.cpp
+++ 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.cpp
@@ -4,22 +4,18 @@
 
 using namespace std;
 
-vector<int> solution(string msg) {
+vector<int> solution(string msg) 
+{
     unordered_map<string, int> dict;
-    for(char ch = 'A'; ch <= 'Z'; ch++)
-        dict[to_string(ch)] = ch - 'A' + 1;
+    for(char ch = 'A'; ch <= 'Z'; ch++) dict[to_string(ch)] = ch - 'A' + 1;
     
     int index = 27;
     vector<int> answer;
     for(int i = 0; i < msg.length(); i++)
     {
         string str = to_string(msg[i]);
-        int j;
-        for(j = i + 1; j < msg.length(); j++)
-        {
-            if(dict[str + msg[j]] == 0) break;
-            str += msg[j];
-        }
+        int j = i + 1;
+        while(j < msg.length() && dict[str + msg[j]] != 0) str += msg[j++];
         
         dict[str + msg[j]] = index++;
         answer.emplace_back(dict[str]);
