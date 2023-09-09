83d204f0d3a0a77bdd3b18f68d9db12cfe1e5c81
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-09-09 17:18:48 +0900

    [level 2] Title: [3차] n진수 게임, Time: 0.39 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.cpp
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.cpp
@@ -5,8 +5,8 @@
 using namespace std;
 
 string solution(int n, int t, int m, int p) {
+    const string digits = "0123456789ABCDEF";
     string numbers = "0"; // default
-	const char digits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
 	for (int i = 1; i < t * m; i++)
 	{
         int num = i;
