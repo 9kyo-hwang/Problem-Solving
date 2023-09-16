f47265e8f89f7cb3bbfe9e5607d84683880ba295
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-09-16 20:37:04 +0900

    [level 2] Title: 괄호 변환, Time: 0.05 ms, Memory: 4.24 MB -BaekjoonHub

--- 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.cpp
+++ 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.cpp
@@ -16,7 +16,7 @@ bool is_correct(const string &u)
             else stack.emplace_back(ch);
         }
     }
-    
+
     return stack.empty();
 }
 
@@ -24,45 +24,59 @@ int get_balanced_index(const string &w)
 {
     int index = 0;
     int open = 0, close = 0;
-    
+
     for(int i = 0; i < w.length(); i++)
     {
         if(w[i] == '(') open++;
         else close++;
-        
+
         if(open == close)
         {
             index = i + 1;
             break;
         }
     }
-    
+
     return index;
 }
 
-string solution(string w) 
+string converter(const string &w)
 {
     // 1
     if(w.empty()) return w;
-    
+
     // 2
     int index = get_balanced_index(w);
     string u = w.substr(0, index);
     string v = w.substr(index);
-    
-    // 3
-    if(is_correct(u)) return u + solution(v);
 
-    // 4
-    
-    // 4-4
-    u = u.substr(1, u.length() - 2); 
-    for(char &ch : u)
+    string result;
+    if(is_correct(u)) // 3
     {
-        if(ch == '(') ch = ')';
-        else ch = '(';
+        // 3-1
+        result = u + converter(v);
     }
-    
-    // 4-5(4-1 + 4-2 + 4-3 + 4-4)
-    return "(" + solution(v) + ")" + u;
+    else // 4
+    {
+        result += '('; // 4-1
+        result += converter(v); // 4-2
+        result += ')'; // 4-3
+
+        // 4-4
+        u = u.substr(1, u.length() - 2); 
+        for(char &ch : u)
+        {
+            if(ch == '(') ch = ')';
+            else ch = '(';
+        }
+
+        // 4-5
+        result += u;
+    }
+    return result;
+}
+
+string solution(string p) 
+{
+    return converter(p);
 }
