007d18467bd6bd56fe41a79c953ee1b66ef264d7
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-09-19 02:24:39 +0900

    [level 2] Title: 순위 검색, Time: 589.55 ms, Memory: 43.1 MB -BaekjoonHub

--- 프로그래머스/lv2/72412. 순위 검색/순위 검색.cpp
+++ 프로그래머스/lv2/72412. 순위 검색/순위 검색.cpp
@@ -5,76 +5,72 @@
 
 using namespace std;
 
-vector<int> solution(vector<string> info, vector<string> query) {
-  unordered_map<string, int> category = {
-      {"cpp", 0}, {"java", 1}, {"python", 2},
-      {"backend", 0}, {"frontend", 1},
-      {"junior", 0}, {"senior", 1},
-      {"chicken", 0}, {"pizza", 1},
-  };
+vector<int> solution(vector<string> info, vector<string> query) 
+{
+    unordered_map<string, int> cat = 
+    {
+        {"cpp", 0}, {"java", 1}, {"python", 2},
+        {"backend", 0}, {"frontend", 1},
+        {"junior", 0}, {"senior", 1},
+        {"chicken", 0}, {"pizza", 1},
+    };
 
-  vector<vector<vector<vector<vector<int>>>>> table(3, vector<vector<vector<vector<int>>>>(2, vector<vector<vector<int>>>(2, vector<vector<int>>(2))));
+    vector<int> table[3][2][2][2];
 
-  for (const auto &str : info) {
-    istringstream ss(str);
-    string buffer;
-    vector<string> buffers;
-    while (getline(ss, buffer, ' '))
-      buffers.emplace_back(buffer);
-    table[category[buffers[0]]][category[buffers[1]]][category[buffers[2]]][category[buffers[3]]].emplace_back(stoi(buffers[4]));
-  }
+    for (const auto &str : info) 
+    {
+        istringstream ss(str); string buffer;
+        vector<string> v;
 
-  vector<int> result;
-  for (const auto &q : query) {
-    istringstream ss(q);
-    string buffer;
-    vector<string> buffers;
-    while (getline(ss, buffer, ' '))
-      buffers.emplace_back(buffer);
+        while (getline(ss, buffer, ' ')) 
+            v.emplace_back(buffer);
 
-    int score = stoi(buffers[7]);
-    int count = 0;
-
-    vector<int> range[4];
-    if (buffers[0] == "-") {
-      range[0] = {0, 1, 2};
-    } else {
-      range[0] = {category[buffers[0]]};
+        table[cat[v[0]]][cat[v[1]]][cat[v[2]]][cat[v[3]]].emplace_back(stoi(v[4]));
     }
 
-    if (buffers[2] == "-") {
-      range[1] = {0, 1};
-    } else {
-      range[1] = {category[buffers[2]]};
-    }
+    vector<int> result;
+    for (const auto &q : query) 
+    {
+        istringstream ss(q); string buffer;
+        vector<string> v;
+        while (getline(ss, buffer, ' '))
+            v.emplace_back(buffer);
 
-    if (buffers[4] == "-") {
-      range[2] = {0, 1};
-    } else {
-      range[2] = {category[buffers[4]]};
-    }
+        int score = stoi(v[7]);
+        int count = 0;
 
-    if (buffers[6] == "-") {
-      range[3] = {0, 1};
-    } else {
-      range[3] = {category[buffers[6]]};
-    }
+        vector<int> range[4];
+        if (v[0] == "-") range[0] = {0, 1, 2};
+        else range[0] = {cat[v[0]]};
 
-    for (const auto &i : range[0]) {
-      for (const auto &j : range[1]) {
-        for (const auto &k : range[2]) {
-          for (const auto &l : range[3]) {
-            for (const auto &s : table[i][j][k][l]) {
-              if (s >= score)
-                count += 1;
+        if (v[2] == "-") range[1] = {0, 1};
+        else range[1] = {cat[v[2]]};
+
+        if (v[4] == "-") range[2] = {0, 1};
+        else range[2] = {cat[v[4]]};
+
+        if (v[6] == "-") range[3] = {0, 1};
+        else range[3] = {cat[v[6]]};
+
+        for (const auto &i : range[0]) 
+        {
+            for (const auto &j : range[1]) 
+            {
+                for (const auto &k : range[2]) 
+                {
+                    for (const auto &l : range[3]) 
+                    {
+                        for (const auto &s : table[i][j][k][l]) 
+                        {
+                            if (s >= score) count += 1;
+                        }
+                    }
+                }
             }
-          }
         }
-      }
-    }
 
-    result.emplace_back(count);
-  }
+        result.emplace_back(count);
+    }
     
     return result;
 }
