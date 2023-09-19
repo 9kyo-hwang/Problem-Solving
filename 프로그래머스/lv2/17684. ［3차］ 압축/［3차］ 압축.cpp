938b1bc5b91adda7289acf283ab27a8f38714918
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-09-19 17:17:40 +0900

    [level 2] Title: [3차] 압축, Time: 0.23 ms, Memory: 4.27 MB -BaekjoonHub

--- 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.cpp
+++ 프로그래머스/lv2/17684. ［3차］ 압축/［3차］ 압축.cpp
@@ -8,18 +8,33 @@ vector<int> solution(string msg)
 {
     unordered_map<string, int> dict;
     for(char ch = 'A'; ch <= 'Z'; ch++) dict[to_string(ch)] = ch - 'A' + 1;
-    
     int index = 27;
-    vector<int> answer;
-    for(int i = 0; i < msg.length(); i++)
+    
+    vector<int> answer; int i = 0;
+    while(i < msg.length())
     {
         string str = to_string(msg[i]);
         int j = i + 1;
-        while(j < msg.length() && dict[str + msg[j]] != 0) str += msg[j++];
+        while(j < msg.length() && dict[str + msg[j]] != 0)
+        {
+            str += msg[j];
+            j++;
+        }
         
-        dict[str + msg[j]] = index++;
+        dict[str + msg[j]] = index; index++;
         answer.emplace_back(dict[str]);
-        i = j - 1;
+        i = j;
     }
+    
+//     for(int i = 0; i < msg.length(); i++)
+//     {
+//         string str = to_string(msg[i]);
+//         int j = i + 1;
+//         while(j < msg.length() && dict[str + msg[j]] != 0) str += msg[j++];
+        
+//         dict[str + msg[j]] = index++;
+//         answer.emplace_back(dict[str]);
+//         i = j - 1;
+//     }
     return answer;
 }
