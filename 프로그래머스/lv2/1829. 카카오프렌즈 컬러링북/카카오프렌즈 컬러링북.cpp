0da83ca974b41fc58c86bfcfc65f95d092a7012e
구교황 <49135176+GuKyoHwang@users.noreply.github.com>
2023-09-19 17:07:35 +0900

    [level 2] Title: 카카오프렌즈 컬러링북, Time: 2.15 ms, Memory: 4.79 MB -BaekjoonHub

--- 프로그래머스/lv2/1829. 카카오프렌즈 컬러링북/카카오프렌즈 컬러링북.cpp
+++ 프로그래머스/lv2/1829. 카카오프렌즈 컬러링북/카카오프렌즈 컬러링북.cpp
@@ -2,33 +2,42 @@
 
 using namespace std;
 
-vector<vector<bool>> visited;
-int count_cells(vector<vector<int>> &p, const int m, const int n, const int c, int x, int y) {
-    if(x < 0 || x >= m || y < 0 || y >= n || visited[x][y]) {
-        return 0;
-    } else if(p[x][y] == 0 || p[x][y] != c) {
-        return 0;
-    } else {
-        visited[x][y] = true;
-        return 1 + count_cells(p, m, n, c, x - 1, y) + count_cells(p, m, n, c, x, y + 1) + count_cells(p, m, n, c, x + 1, y) + count_cells(p, m, n, c, x, y - 1);
+constexpr int OFFSET[][2] = 
+{
+    {-1, 0},
+    {0, 1},
+    {1, 0},
+    {0, -1},
+};
+
+int count_cells(vector<vector<int>> &p, const int m, const int n, const int c, int x, int y) 
+{
+    if(x < 0 || x >= m || y < 0 || y >= n || p[x][y] == -1) return 0;
+    else if(p[x][y] == 0 || p[x][y] != c) return 0;
+    else 
+    {
+        p[x][y] = -1;
+        int cnt = 1;
+        for(auto &dir : OFFSET)
+        {
+            cnt += count_cells(p, m, n, c, x + dir[0], y + dir[1]);
+        }
+        return cnt;
     }
 }
 
 vector<int> solution(int m, int n, vector<vector<int>> picture) {
-    visited = vector(m, vector(n, false));
     int cnt = 0;
     int max_cells = 0;
-    for(int x = 0; x < m; x++) {
-        for(int y = 0; y < n; y++) {
-            int c = picture[x][y];
-            int result = count_cells(picture, m, n, c, x, y);
-            if(result == 0)
-                continue;
+    for(int x = 0; x < m; x++) 
+    {
+        for(int y = 0; y < n; y++) 
+        {
+            int result = count_cells(picture, m, n, picture[x][y], x, y);
+            if(result == 0) continue;
             
             cnt += 1;
-            if(result > max_cells) {
-                max_cells = result;
-            }
+            if(result > max_cells) max_cells = result;
         }
     }
     
