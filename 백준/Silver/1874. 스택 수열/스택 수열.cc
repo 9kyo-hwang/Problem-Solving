0c9f7c6d73788c913fe0145e6b5b5264eddca7f5
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2023-11-08 23:04:41 +0900

    [Silver II] Title: 스택 수열, Time: 20 ms, Memory: 2656 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.cc
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -1,42 +1,49 @@
 #include <iostream>
 #include <vector>
-#include <stack>
-#include <queue>
 
 using namespace std;
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr), cout.tie(nullptr);
+int main() 
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
 
-    int N;
-    cin >> N;
+	int N; cin >> N;
+	
+	vector<int> stack;
+	vector<char> operators;
 
-    vector<int> v(N); // sequence of popped
-    for(auto &i : v)
-        cin >> i;
-    
-    stack<int> s; // number stack
-    queue<char> q; // operator queue
-    int i = 0;
-    for(auto n = 1; n <= N; n++) {
-        s.emplace(n);
-        q.emplace('+'); // push
+	int num = 0;
+	bool flag = true;
+	while (N--) 
+	{
+		int n; cin >> n;
+		while (num < n) 
+		{
+			num++;
+			stack.emplace_back(num);
+			operators.emplace_back('+');
+		}
 
-        while(!s.empty() && s.top() == v[i]) { // pop number
-            s.pop();
-            q.emplace('-'); // popped
-            i++;
-        }
-    }
+		if (stack.back() == n) 
+		{
+			stack.pop_back();
+			operators.emplace_back('-');
+		}
+		else
+		{
+			flag = false;
+			break;
+		}
+	}
 
-    if(!s.empty()) cout << "NO";
-    else {
-        while(!q.empty()) {
-            cout << q.front() << "\n";
-            q.pop();
-        }
-    }
+	if (!flag) cout << "NO";
+	else 
+	{
+		for (const char& ch : operators)
+			cout << ch << "\n";
+	}
 
-    return 0;
+	return 0;
 }
