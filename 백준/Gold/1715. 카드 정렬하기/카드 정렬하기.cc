a9f0d1c19a8c50965cfa0fff0481fba6b5040522
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2023-11-08 15:15:03 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 20 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.cc
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.cc
@@ -2,7 +2,6 @@
 #include <queue>
 
 using namespace std;
-using ll = long long;
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -10,20 +9,20 @@ int main() {
 	cout.tie(nullptr);
 
 	int N; cin >> N;
-	priority_queue<ll, vector<ll>, greater<>> pq;
+	priority_queue<int, vector<int>, greater<>> pq;
 	while (N--) {
 		int n; cin >> n;
 		pq.emplace(n);
 	}
 
-	ll answer = 0;
+	int answer = 0;
 	while (!pq.empty()) {
-		ll A = pq.top(); pq.pop();
+		int A = pq.top(); pq.pop();
 
 		if (pq.empty())
 			break;
 
-		ll B = pq.top(); pq.pop();
+		int B = pq.top(); pq.pop();
 
 		answer += A + B;
 		pq.emplace(A + B);
