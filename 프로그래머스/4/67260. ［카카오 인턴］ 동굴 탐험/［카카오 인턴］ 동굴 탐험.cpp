49379264b48acbef80011b3fa15d5e4f53eb8d6f
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2023-11-10 02:19:53 +0900

    [level 4] Title: [카카오 인턴] 동굴 탐험, Time: 50.08 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/4/67260. ［카카오 인턴］ 동굴 탐험/［카카오 인턴］ 동굴 탐험.cpp
@@ -0,0 +1,80 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+vector<vector<int>> getDirectedGraph(const int n, const vector<vector<int>> &graph) {
+    vector<vector<int>> directedGraph(n);
+    vector<bool> visited(n, false);
+    queue<int> q;
+    
+    q.emplace(0);
+    visited[0] = true;
+    while(!q.empty()) {
+        int from = q.front();
+        q.pop();
+        
+        for(const int to : graph[from]) {
+            if(visited[to]) continue;
+            
+            visited[to] = true;
+            directedGraph[from].emplace_back(to);
+            q.emplace(to);
+        }
+    }
+    
+    return directedGraph;
+}
+
+bool canExplore(const int n, vector<vector<int>> &directedGraph, vector<vector<int>> &order) {
+    vector<int> indegree(n, 0);
+    for(const auto &from : directedGraph) {
+        for(const auto &to : from) {
+            indegree[to]++;
+        }
+    }
+    
+    for(const auto &o : order) {
+        int A = o[0], B = o[1];
+        directedGraph[A].emplace_back(B);
+        indegree[B]++;
+    }
+    
+    queue<int> q;
+    for(const int in : indegree) {
+        if(in == 0)
+            q.emplace(in);
+    }
+    
+    for(int i = 0; i < n; i++) {
+        if(q.empty())
+            return false;
+        
+        int from = q.front();
+        q.pop();
+        
+        for(const int to : directedGraph[from]) {
+            indegree[to]--;
+            if(indegree[to] == 0) {
+                q.emplace(to);
+            }
+        }
+    }
+    
+    return true;
+}
+
+bool solution(int n, vector<vector<int>> path, vector<vector<int>> order) {
+    vector<vector<int>> graph(n);
+    for(const auto &p : path) {
+        graph[p[0]].emplace_back(p[1]);
+        graph[p[1]].emplace_back(p[0]);
+    }
+    
+    auto directedGraph = getDirectedGraph(n, graph);
+    auto result = canExplore(n, directedGraph, order);
+    
+    return result;
+}
