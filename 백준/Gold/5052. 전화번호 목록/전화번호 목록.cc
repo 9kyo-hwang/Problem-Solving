9bc6a3ae8e0605a80684d41b8be29bddbd07c73c
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2023-11-14 22:56:55 +0900

    [Gold IV] Title: 전화번호 목록, Time: 224 ms, Memory: 139168 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.cc
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.cc
@@ -1,81 +1,73 @@
 #include <iostream>
 #include <vector>
-#include <string>
-#include <cstring>
 #include <algorithm>
-#include <functional>
 
 using namespace std;
 
-int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
-  cout.tie(nullptr);
+class Trie {
+ public:
+  Trie() : root(new Node()) {}
 
-  auto CheckFtn = [&](const vector<vector<string>> &v) {
-    for (const auto &numbers : v) {
-      auto size = numbers.size();
-      if(size == 0)
-        continue;
-
-      for (int i = 0; i < size - 1; i++) {
-        auto str = numbers[i].c_str();
-        auto len = numbers[i].length();
-        for (int j = i + 1; j < size; j++) {
-          if (strncmp(str, numbers[j].c_str(), len) == 0) {
-            return false;
-          }
-        }
-      }
+  void insert(const string &str) { 
+    Node *p = root;
+    for (const char &ch : str) {
+      int i = ch - '0';
+      if (!p->next[i]) p->next[i] = new Node;
+      p = p->next[i];
     }
+    p->leaf = true;
+  }
 
-    return true;
+  bool search(const string& str) {
+    Node *p = root;
+    for (const char &ch : str) {
+      int i = ch - '0';
+      if (!p->next[i]) return false;
+
+      p = p->next[i];
+      if (p->leaf) return true;
+    }
+    return false;
+  }
+
+ private:
+  struct Node {
+    bool leaf;
+    vector<Node *> next;
+
+    Node() : leaf(false) { next.assign(10, nullptr); }
   };
 
-  int t;
-  cin >> t;
+  Node *root;
+};
 
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr), cout.tie(nullptr);
+
+  int t; cin >> t;
+  
   while (t--) {
-    int n;
-    cin >> n;
-    vector<vector<string>> phone_numbers(10);
+    Trie *trie = new Trie;
 
-    while (n--) {
-      string phone_number;
-      cin >> phone_number;
+    int n; cin >> n;
+    vector<string> phone_numbers(n);
+    for (string &phone : phone_numbers) {
+      cin >> phone;
+    }
+    sort(phone_numbers.begin(), phone_numbers.end());
 
-      switch (phone_number[0] - '0') {
-        case 0: phone_numbers[0].emplace_back(phone_number);
-          break;
-        case 1: phone_numbers[1].emplace_back(phone_number);
-          break;
-        case 2: phone_numbers[2].emplace_back(phone_number);
-          break;
-        case 3: phone_numbers[3].emplace_back(phone_number);
-          break;
-        case 4: phone_numbers[4].emplace_back(phone_number);
-          break;
-        case 5: phone_numbers[5].emplace_back(phone_number);
-          break;
-        case 6: phone_numbers[6].emplace_back(phone_number);
-          break;
-        case 7: phone_numbers[7].emplace_back(phone_number);
-          break;
-        case 8: phone_numbers[8].emplace_back(phone_number);
-          break;
-        case 9: phone_numbers[9].emplace_back(phone_number);
-          break;
-        default: break;
+    bool flag = true;
+    for (const string &phone : phone_numbers) {
+      if (trie->search(phone)) {
+        flag = false;
+        break;
       }
+      trie->insert(phone);
     }
 
-    for (auto &numbers : phone_numbers) {
-      sort(numbers.begin(), numbers.end(), [](const string &a, const string &b) {
-        return a.length() < b.length();
-      });
-    }
-
-    cout << (CheckFtn(phone_numbers) ? "YES" : "NO") << "\n";
+    cout << (flag ? "YES" : "NO") << "\n";
+    delete trie;
   }
 
   return 0;
