144951bce6a7442b36dbd47e5464c36fbc20b1fc
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2023-11-20 23:49:38 +0900

    [Silver V] Title: 성적 통계, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/5800. 성적 통계/성적 통계.py
+++ 백준/Silver/5800. 성적 통계/성적 통계.py
@@ -2,12 +2,11 @@ input = open(0).readline
 
 K = int(input())
 for X in range(1, K + 1):
-    N = list(map(int, input().split()))
-    grade = sorted(N[1:])
+    N = sorted(list(map(int, input().split()))[1:])
     
-    maxi = max(grade)
-    mini = min(grade)
-    gap = max([grade[i + 1] - grade[i] for i in range(len(grade) - 1)])
+    maxi = max(N)
+    mini = min(N)
+    gap = max([N[i + 1] - N[i] for i in range(len(N) - 1)])
     
     print('Class {}'.format(X))
     print('Max {}, Min {}, Largest gap {}'.format(maxi, mini, gap))
