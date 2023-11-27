21e223e07f6b24591ac9b832662d33f667b6bfe8
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2023-11-27 17:09:03 +0900

    [Silver III] Title: 전쟁 - 땅따먹기, Time: 8152 ms, Memory: 58560 KB -BaekjoonHub

---
+++ 백준/Silver/1270. 전쟁 － 땅따먹기/전쟁 － 땅따먹기.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+input = open(0).readline
+
+n = int(input())
+
+for _ in range(n):
+    info = list(map(int, input().split()))
+    land, soldier = info[0], Counter(info[1:])
+    
+    most_common = Counter(soldier).most_common(1)
+    if most_common[0][1] > land // 2:
+        print(most_common[0][0])
+    else:
+        print('SYJKGW')
