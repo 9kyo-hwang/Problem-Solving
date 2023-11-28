e22a6dcd3acce9fe6a0c7b554ad827334b85ffa4
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2023-11-28 16:33:32 +0900

    [Silver II] Title: DNA 비밀번호, Time: 12 ms, Memory: 1968 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.cc
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.cc
@@ -1,58 +1,39 @@
-#include <iostream>
-#include <unordered_map>
-#include <queue>
+#include <cstdio>
 
-using namespace std;
+#define MAX 1000001
+
+int counts[91];
+int satisfied[91];
+
+inline bool is_satisfied() {
+  return counts['A'] >= satisfied['A'] && counts['C'] >= satisfied['C'] &&
+         counts['G'] >= satisfied['G'] && counts['T'] >= satisfied['T'];
+}
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-
   int S, P;
-  cin >> S >> P;
+  scanf(" %d %d", &S, &P);
 
-  string DNA;
-  cin >> DNA;
+  char str[MAX];
+  scanf(" %s", str);
 
-  unordered_map<char, int> counts = {
-      {'A', 0},
-      {'C', 0},
-      {'G', 0},
-      {'T', 0},
-  };
-
-  deque<char> deq;
   for (int i = 0; i < P; i++) {
-    deq.emplace_back(DNA[i]);
-    counts[DNA[i]] += 1;
+    counts[str[i]] += 1;
   }
 
-  int satisfied_count[4]{};
-  for (int i = 0; i < 4; i++) {
-    cin >> satisfied_count[i];
-  }
-  
-  auto is_satisfied = [&]() {
-    return counts['A'] >= satisfied_count[0] &&
-           counts['C'] >= satisfied_count[1] &&
-           counts['G'] >= satisfied_count[2] &&
-           counts['T'] >= satisfied_count[3];
-  };
+  scanf(" %d %d %d %d", &satisfied['A'], &satisfied['C'], &satisfied['G'],
+        &satisfied['T']);
 
   int answer = is_satisfied();
-  for (int i = P; i < S; i++) {
-    char ch = deq.front();
-    counts[ch] -= 1;
-    deq.pop_front();
 
-    ch = DNA[i];
-    counts[ch] += 1;
-    deq.emplace_back(ch);
+  for (int i = 0, j = P; j < S; i++, j++) {
+    counts[str[i]] -= 1;
+    counts[str[j]] += 1;
 
     answer += is_satisfied();
   }
 
-  cout << answer;
+  printf("%d", answer);
 
   return 0;
 }
