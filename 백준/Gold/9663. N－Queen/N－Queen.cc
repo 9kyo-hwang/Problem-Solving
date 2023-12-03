697fcdf78a00edac5b183f9e4f24993ef49f42b1
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2023-12-03 21:55:11 +0900

    [Gold IV] Title: N-Queen, Time: 4572 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.cc
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -1,43 +1,45 @@
-#include <cstdio>
-#include <cstdlib>
+#include <iostream>
+#include <vector>
 
-#define MAX 15
+using namespace std;
 
-int queens(int);
-bool promising(int);
-
-int cols[MAX + 1]; 
 int N;
-
-int main() {
-    scanf("%d", &N);
-    printf("%d", queens(0));
-
-    return 0;
-}
-
-int queens(int level) {
-    if(!promising(level))
-        return 0;
-    else if(level == N) {
-        return 1;
-    }
-    else {
-        int count = 0;
-        for(int i = 1; i <= N; i++) {
-            cols[level + 1] = i;
-            count += queens(level + 1);
-        }
-        return count;
-    }
-}
+vector<int> cols;
 
 bool promising(int level) {
-    for(int i = 1; i < level; i++) {
-        if(cols[i] == cols[level])
-            return false;
-        else if(level - i == abs(cols[level] - cols[i]))
-            return false;
+  for (int i = 1; i <= level - 1; i++) {
+    if (cols[i] == cols[level]) {
+      return false;
+    } else if (level - i == abs(cols[level] - cols[i])) {
+      return false;
     }
-    return true;
+  }
+  return true;
+}
+
+int queens(int level = 0) {
+  if (!promising(level)) {
+    return 0;
+  } else if (level == N) {
+    return 1;
+  } else {
+    int count = 0;
+    for (int i = 1; i <= N; i++) {
+      cols[level + 1] = i;
+      count += queens(level + 1);
+    }
+    return count;
+  }
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  cin >> N;
+  cols.assign(N + 1, 0);
+
+  cout << queens();
+
+  return 0;
 }
