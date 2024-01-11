3d9c8ab60269ac9b178fb460847c351ba6cbbd4e
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2024-01-11 15:45:31 +0900

    [Silver II] Title: DFS와 BFS, Time: 84 ms, Memory: 34052 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -4,16 +4,17 @@ input = open(0).readline
 
 N, M, V = map(int, input().split())
 
-graph = [list() for _ in range(N + 1)]
+graph = [[] for _ in range(N + 1)]
+
 for _ in range(M):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
     
-for i in range(1, N + 1):
-    graph[i].sort()
-    
+[graph[i].sort() for i in range(1, N + 1)]
+
 visited = [False] * (N + 1)
+
 
 def dfs(v: int = V):
     if visited[v]:
@@ -30,6 +31,7 @@ dfs()
 print()
 
 visited = [False] * (N + 1)
+
 
 def bfs(v: int = V):
     dq = deque([v])
