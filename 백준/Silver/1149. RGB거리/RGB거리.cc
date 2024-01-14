15660bccf87df28b4aa0a59abe82e74b01286856
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2024-01-14 17:35:04 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2164 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -11,20 +11,19 @@ int main() {
   int N;
   cin >> N;
 
-  vector costs(N, vector(3, 0));
-  for (auto &cost : costs) {
-    for (auto &color : cost) {
-      cin >> color;
-    }
-  }
-
   vector dp(N, vector(3, 0));
-  dp[0] = costs[0];
+  for (int i = 0; i < N; ++i) {
+    int r, g, b;
+    cin >> r >> g >> b;
 
-  for (int i = 1; i < N; ++i) {
-    dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + costs[i][0];
-    dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + costs[i][1];
-    dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + costs[i][2];
+    if (i == 0) {
+      dp[i] = {r, g, b};
+      continue;
+    }
+
+    dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + r;
+    dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + g;
+    dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + b;
   }
 
   cout << min({dp[N - 1][0], dp[N - 1][1], dp[N - 1][2]});
