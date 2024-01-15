5d428a4b5a8084f0f1613619e02505496f8f07f3
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2024-01-16 00:40:44 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 440 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -0,0 +1,29 @@
+input = open(0).readline
+INF = float('inf')
+
+N, M = map(int, input().split())
+
+dp = [[INF] * (N + 1) for _ in range(N + 1)]
+for i in range(1, N + 1):
+    dp[i][i] = 0  # self distance = 0
+    
+for _ in range(M):
+    A, B = map(int, input().split())
+    dp[A][B] = dp[B][A] = 1  # A -> B = B -> A
+    
+
+for k in range(1, N + 1):  # via node
+    for i in range(1, N + 1):
+        for j in range(1, N + 1):
+            # distance(i -> j) vs distance(i -> k) + distance(k -> j)
+            # min: update unconnected(INF) node to connected
+            dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j])
+            
+
+min_cnt, min_idx = INF, 0
+for i in range(1, N + 1):
+    if sum(dp[i][1:]) < min_cnt:
+        min_cnt = sum(dp[i][1:])
+        min_idx = i
+        
+print(min_idx)
