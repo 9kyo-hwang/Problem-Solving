2a11f09e2ea6e0eb9e7b7c79938e7148b4b54cb0
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2024-01-15 16:42:44 +0900

    [Gold IV] Title: 서강그라운드, Time: 152 ms, Memory: 110840 KB -BaekjoonHub

--- 백준/Gold/14938. 서강그라운드/서강그라운드.py
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.py
@@ -1,32 +1,33 @@
-from heapq import *
-
 input = open(0).readline
-
-n, m, r = map(int, input().split())
-items = [0] + list(map(int, input().split()))
-graph = [[] for _ in range(n + 1)]
-
-for _ in range(r):
-    a, b, l = map(int, input().split())
-    graph[a].append((b, l))
-    graph[b].append((a, l))
+INF = float('inf')
 
 
-def dijkstra(idx):
-    heap = []
-    dist = [float('inf')] * (n + 1)
+def main():
+    n, m, r = map(int, input().split())
+    items = [0] + list(map(int, input().split()))
     
-    heappush(heap, [0, idx])
-    dist[idx] = 0
+    dp = [[INF] * (n + 1) for _ in range(n + 1)]
+    for i in range(1, n + 1):
+        dp[i][i] = 0
+        
+    for _ in range(r):
+        a, b, l = map(int, input().split())
+        dp[a][b] = dp[b][a] = l
+        
+    for k in range(1, n + 1):
+        for i in range(1, n + 1):
+            for j in range(1, n + 1):
+                dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j])
+                
+    ans = 0
+    for i in range(1, n + 1):
+        total = 0
+        for j in range(1, n + 1):
+            if dp[i][j] <= m:
+                total += items[j]
+        ans = max(ans, total)
     
-    while heap:
-        src_dist, src = heappop(heap)
-        for dst, dst_dist in graph[src]:
-            if src_dist + dst_dist < dist[dst]:
-                dist[dst] = src_dist + dst_dist
-                heappush(heap, [dist[dst], dst])
-    
-    return sum(items[i] for i in range(1, n + 1) if dist[i] <= m)
+    print(ans)
 
-
-print(max(dijkstra(i) for i in range(1, n + 1)))
+if __name__ == "__main__":
+    main()
