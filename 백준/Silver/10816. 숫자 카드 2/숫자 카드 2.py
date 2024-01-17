80c6292799c705344e341739de606292dd85e3e0
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2024-01-17 11:07:08 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 808 ms, Memory: 157096 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,16 +1,15 @@
+from collections import defaultdict
+
 input = open(0).readline
-
-mid = int(1e7)
-cvt = lambda x : x + mid
-
-num_cards = [0] * (2 * mid + 1)
 
 N = int(input())
 cards = list(map(int, input().split()))
+
+counter = defaultdict(int)
 for card in cards:
-    num_cards[cvt(card)] += 1
+    counter[card] += 1
 
 M = int(input())
 commands = list(map(int, input().split()))
-for cmd in commands:
-    print(num_cards[cvt(cmd)], end=' ')
+
+print(' '.join(str(counter[cmd]) for cmd in commands))
