7a1da6fa64b0565940fccc5c71d624545198a1b1
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2024-01-19 02:05:29 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 3.64 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -1,25 +1,27 @@
 #include <string>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    deque<int> progress_q(progresses.begin(), progresses.end()), speed_q(speeds.begin(), speeds.end());
     vector<int> answer;
-    for(int i = 0; i < progresses.size(); i++) {
-        int day = 0;
-        while(progresses[i] < 100) {
-            progresses[i] += speeds[i];
-            day++;
-        }
-        for(int j = i + 1; j < speeds.size(); j++)
-            progresses[j] += speeds[j] * day;
-        
-        int index = i;
-        while(index < progresses.size() && progresses[index] >= 100)
-            index++;
-        answer.emplace_back(index - i);
-        i = index - 1; // index는 size를 나타내게 되므로, i++까지 고려해 -2
-    }
     
+    int day = 0, count = 0;
+    while(!progress_q.empty()) {
+        if(progress_q.front() + day * speed_q.front() >= 100) {
+            progress_q.pop_front();
+            speed_q.pop_front();
+            count += 1;
+        } else {
+            if(count > 0) {
+                answer.emplace_back(count);
+                count = 0;
+            }
+            day += 1;
+        }
+    }
+    answer.emplace_back(count);
     return answer;
 }
