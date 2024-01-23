82c9e00d656a29e84a62d2337fec0549fbc411c5
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2024-01-23 22:01:22 +0900

    [Silver I] Title: 언더프라임, Time: 2448 ms, Memory: 32684 KB -BaekjoonHub

--- 백준/Silver/1124. 언더프라임/언더프라임.py
+++ 백준/Silver/1124. 언더프라임/언더프라임.py
@@ -1,29 +1,32 @@
 input = open(0).readline
 
-a, b = map(int, input().split())
-primeCheck = [0, 0] + [1] * (b-1)
-for i in range(2, b+1):
-    if primeCheck[i] == 0:
+A, B = map(int, input().split())
+
+is_primes = [False, False] + [True] * (B - 1)
+for i in range(2, int(B**0.5) + 1):
+    if not is_primes[i]:
         continue
+        
+    for j in range(i * i, B + 1, i):
+        is_primes[j] = False
 
-    for j in range(i*i, b+1, i):
-        primeCheck[j] = 0
 
-ans = 0
-for p in range(a, b+1):
+ans = 0  
+for n in range(A, B + 1):
     cnt = 0
-    k = 2
-    while k <= int(p**0.5):
-        if p % k == 0:
-            p //= k
-            cnt += 1
+    p = 2
+    while p <= int(n**0.5):
+        if n % p != 0:
+            p += 1
         else:
-            k += 1
-
+            cnt += 1
+            n //= p
+            
     if p > 1:
         cnt += 1
-
-    if primeCheck[cnt] == 1:
+    
+    if is_primes[cnt]:
         ans += 1
 
+        
 print(ans)
