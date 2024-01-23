202f0ef5795e2661f6a143e69a3ba64d6e8790db
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2024-01-23 22:02:20 +0900

    [Silver I] Title: 언더프라임, Time: 600 ms, Memory: 111380 KB -BaekjoonHub

--- 백준/Silver/1124. 언더프라임/언더프라임.py
+++ 백준/Silver/1124. 언더프라임/언더프라임.py
@@ -1,32 +1,30 @@
+import math
 input = open(0).readline
 
 A, B = map(int, input().split())
 
-is_primes = [False, False] + [True] * (B - 1)
-for i in range(2, int(B**0.5) + 1):
-    if not is_primes[i]:
-        continue
-        
-    for j in range(i * i, B + 1, i):
-        is_primes[j] = False
+is_primes = [True] * (B + 1)
+is_primes[0] = is_primes[1] = False
 
-
+for i in range(2, int(math.sqrt(B)) + 1):
+    if is_primes[i]:
+        for j in range(i * i, B + 1, i):
+            is_primes[j] = False
+            
 ans = 0  
-for n in range(A, B + 1):
-    cnt = 0
-    p = 2
-    while p <= int(n**0.5):
-        if n % p != 0:
-            p += 1
+for x in range(A, B + 1):
+    sqrt_x = int(math.sqrt(x))
+    div, cnt = 2, 0
+    while div <= sqrt_x:
+        if x % div != 0:
+            div += 1
         else:
             cnt += 1
-            n //= p
+            x //= div
             
-    if p > 1:
+    if x > 1:
         cnt += 1
-    
-    if is_primes[cnt]:
-        ans += 1
-
         
+    ans += 1 if is_primes[cnt] else 0
+    
 print(ans)
