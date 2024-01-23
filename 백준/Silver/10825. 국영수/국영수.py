98a87783f3b74018f976c016d0c2b686429efc9a
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2024-01-24 00:06:53 +0900

    [Silver IV] Title: 국영수, Time: 420 ms, Memory: 87492 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.py
+++ 백준/Silver/10825. 국영수/국영수.py
@@ -3,4 +3,4 @@ input = open(0).readline
 N = int(input())
 scores = [input().split() for _ in range(N)]
 scores.sort(key=lambda x : (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
-print(*[name for name, _, _, _ in scores], sep='\n')
+print(*map(lambda x: x[0], scores), sep='\n')
