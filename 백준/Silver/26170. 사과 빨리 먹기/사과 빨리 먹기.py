d2303f91b4ec43b4510d149786f0431f093c248d
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2024-02-05 09:32:23 +0900

    [Silver III] Title: 사과 빨리 먹기, Time: 112 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/26170. 사과 빨리 먹기/사과 빨리 먹기.py
+++ 백준/Silver/26170. 사과 빨리 먹기/사과 빨리 먹기.py
@@ -5,28 +5,31 @@ r, c = map(int, input().split())
 
 offset = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 visited = [[col == -1 for col in row] for row in board]
+visited[r][c] = True
 
 
 def out_of_bound(x: int, y: int) -> bool:
     return not(0 <= x <= 4 and 0 <= y <= 4)
 
 
-def dfs(x: int, y: int, step: int = 0, apple: int = 0) -> int:
-    if out_of_bound(x, y) or visited[x][y]:
-        return -1
-    elif apple == 2 and board[x][y] == 1:
-        return step
-    else:
-        visited[x][y] = True
-        min_step: int = -1
-        for dx, dy in offset:
-            nx, ny = x + dx, y + dy
-            
-            res = dfs(nx, ny, step + 1, apple + board[x][y])
-            if res != -1 and (min_step == -1 or res < min_step):
-                min_step = res
-            
-        visited[x][y] = False
-        return min_step
+ans = float('inf')
+def dfs(x: int, y: int, step: int = 0, apple: int = 0):
+    global ans
+    
+    if apple == 3:
+        ans = min(ans, step)
+        return
+    
+    
+    for dx, dy in offset:
+        nx, ny = x + dx, y + dy
+        if out_of_bound(nx, ny) or visited[nx][ny]:
+            continue
+        
+        visited[nx][ny] = True
+        dfs(nx, ny, step + 1, apple + board[nx][ny])
+        visited[nx][ny] = False
 
-print(dfs(r, c))
+
+dfs(r, c)
+print(-1 if ans == float('inf') else ans)
