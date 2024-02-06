aefd1d858949cb33773141d91345474e7ec73582
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2024-02-06 18:28:15 +0900

    [Silver I] Title: 숨바꼭질, Time: 180 ms, Memory: 114860 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,14 +1,23 @@
+from collections import deque
+
 input = open(0).readline
 
-def dfs(n: int, k: int) -> int:
-    if k <= n:  # only X - 1
-        return n - k
-    elif k == 1:  # cannot go back more
-        return 1
-    elif k % 2 == 0:  # X + 1 vs X * 2
-        return min(k - n, dfs(n, k // 2) + 1)
-    else:  # X - 1 vs X + 1
-        return min(dfs(n, k + 1) + 1, dfs(n, k - 1) + 1)
-
 N, K = map(int, input().split())
-print(dfs(N, K))
+visited = [-1] * 100001
+
+visited[N] = 0
+dq = deque([N])
+
+while dq:
+    pos = dq.popleft()
+    
+    if pos == K:
+        print(visited[pos])
+        break
+    
+    for nxt in [pos - 1, pos + 1, pos * 2]:
+        if not(0 <= nxt <= 100000) or visited[nxt] != -1:
+            continue
+        
+        visited[nxt] = visited[pos] + 1
+        dq.append(nxt)
