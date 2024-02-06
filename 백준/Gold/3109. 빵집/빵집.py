acd6b32d798089d2908986596818573ea15553f6
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2024-02-06 15:59:17 +0900

    [Gold II] Title: 빵집, Time: 1228 ms, Memory: 342068 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.py
+++ 백준/Gold/3109. 빵집/빵집.py
@@ -1,10 +1,7 @@
-import sys
-input = sys.stdin.readline
+input = open(0).readline
 
 R, C = map(int, input().split())
 board = [list(input().rstrip()) for _ in range(R)]
-visited = [[False] * C for _ in range(R)]
-offset = [(-1, 1), (0, 1), (1, 1)]
 
 
 def out_of_bound(x: int, y: int) -> bool:
@@ -12,18 +9,13 @@ def out_of_bound(x: int, y: int) -> bool:
 
 
 def dfs(x: int, y: int) -> bool:
-    if out_of_bound(x, y) or board[x][y] == 'x' or visited[x][y]:
+    if out_of_bound(x, y) or board[x][y] == 'x':
         return False
     elif y == C - 1:
         return True
     else:
-        visited[x][y] = True
-        for dx, dy in offset:
-            nx, ny = x + dx, y + dy
-            if dfs(nx, ny):
-                return True
-
-        return False
+        board[x][y] = 'x'
+        return dfs(x - 1, y + 1) or dfs(x, y + 1) or dfs(x + 1, y + 1)
         
         
 print(sum(dfs(i, 0) for i in range(R)))
