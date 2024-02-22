8f4d0aeff1e15d001d8c9eb746220d9a3fa7928e
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2024-02-22 18:58:21 +0900

    [Silver IV] Title: 수 찾기, Time: 96 ms, Memory: 50372 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,4 +1,4 @@
 N = int(input())
-A = set(map(int, input().split()))
+A = set(input().split())
 M = int(input())
-print('\n'.join('1' if num in A else '0' for num in map(int, input().split())))
+print('\n'.join('1' if num in A else '0' for num in input().split()))
