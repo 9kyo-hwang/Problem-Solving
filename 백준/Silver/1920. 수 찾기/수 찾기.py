d5ccaef6a86d31e0e7432b491b4c59bbed89a9c3
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2024-02-22 18:57:59 +0900

    [Silver IV] Title: 수 찾기, Time: 124 ms, Memory: 47324 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,10 +1,4 @@
-input = open(0).readline
-
-N = input()  # 집합에 저장될 수들의 개수
-A = set(list(map(int, input().split())))  # 집합에 저장될 N개의 자연수들
-
-M = input()  # 찾고자 하는 수의 개수
-B = list(map(int, input().split()))  # M개의 찾고자 하는 수
-
-result = [1 if i in A else 0 for i in B]
-print(*result, sep='\n')
+N = int(input())
+A = set(map(int, input().split()))
+M = int(input())
+print('\n'.join('1' if num in A else '0' for num in map(int, input().split())))
