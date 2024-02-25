0d09291bca097bf2c65bf896bf5834ff1f5ef6d9
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2024-02-25 17:23:08 +0900

    [Silver II] Title: 촌수계산, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.py
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -1,26 +1,24 @@
 input = open(0).readline
 
 n = int(input())
-tree = [[] for _ in range(n + 1)]
-visited = [False] * (n + 1)
-
 a, b = map(int, input().split())
-for _ in range(int(input())):
+m = int(input())
+
+tree = [[] for _ in range(n + 1)]
+dists = [-1] * (n + 1)
+dists[a] = 0
+
+for _ in range(m):
     x, y = map(int, input().split())
     tree[x].append(y)
     tree[y].append(x)
     
 
-def dfs(src: int = a, dist: int = 0):
-    if visited[src]:
-        return -1
-    elif src == b:
-        return dist
-    else:
-        visited[src] = True
-        ans = -1
-        for dst in tree[src]:
-            ans = max(ans, dfs(dst, dist + 1))
-        return ans
+def dfs(src: int = a):
+    for dst in tree[src]:
+        if dists[dst] == -1:
+            dists[dst] = dists[src] + 1
+            dfs(dst)
         
-print(dfs())
+dfs()
+print(dists[b])
