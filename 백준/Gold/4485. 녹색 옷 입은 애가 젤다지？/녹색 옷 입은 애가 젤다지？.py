43ed15da1c54ee1c1fdba582bba2d8da53e1c4e0
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2024-03-05 01:22:24 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 108 ms, Memory: 33188 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -1,30 +1,27 @@
-from heapq import heappush, heappop
-
+from heapq import *
 input = open(0).readline
 
 offset = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+cnt = 1
 
-i = 1
-while (N := int(input())) and N != 0:
+while (N := int(input())):
     cave = [list(map(int, input().split())) for _ in range(N)]
-    visited = [[False] * N for _ in range(N)]
     
-    heap = []
-    heappush(heap, (cave[0][0], 0, 0))
-    visited[0][0] = True
+    heapify(heap := [(cave[0][0], 0, 0)])
+    cave[0][0] = -1
     
     while heap:
         cost, x, y = heappop(heap)
         if x == N - 1 and y == N - 1:
-            print("Problem {}: {}".format(i, cost))
+            print("Problem {}: {}".format(cnt, cost))
             break
         
         for dx, dy in offset:
             nx, ny = x + dx, y + dy
-            if not(0 <= nx < N and 0 <= ny < N) or visited[nx][ny]:
+            if not (0 <= nx < N and 0 <= ny < N) or cave[nx][ny] == -1:
                 continue
-                
-            visited[nx][ny] = True
-            heappush(heap, (cost + cave[nx][ny], nx, ny))
             
-    i += 1
+            heappush(heap, (cost + cave[nx][ny], nx, ny))
+            cave[nx][ny] = -1
+    
+    cnt += 1
