15c5a00113868fe97133f890553d75859e5666bd
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2024-03-05 01:28:00 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 80 ms, Memory: 33188 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -1,16 +1,14 @@
-import heapq
-
+from heapq import *
 input = open(0).readline
 
 def dijkstra(size):
-    heapq.heapify(hq := [(cave[0][0], 0, 0)])
+    heapify(hq := [(cave[0][0], 0, 0)])
     cave[0][0] = -1
     
     while hq:
-        cost, x, y = heapq.heappop(hq)
+        cost, x, y = heappop(hq)
         for dx, dy in [(-1,0),(0,1),(1,0),(0,-1)]:
-            nx = x+dx
-            ny = y+dy
+            nx, ny = x+dx, y+dy
             
             if nx == ny == n-1:
                 return cost+cave[nx][ny]
@@ -18,11 +16,10 @@ def dijkstra(size):
             if nx < 0 or ny < 0 or nx == size or ny == size or cave[nx][ny] == -1:
                 continue
 
-            heapq.heappush(hq, (cost+cave[nx][ny],nx,ny))
+            heappush(hq, (cost+cave[nx][ny],nx,ny))
             cave[nx][ny] = -1
     
 cnt = 0
-
 while (n:=int(input())):
     cnt += 1
     cave = [list(map(int,input().split())) for _ in range(n)]
