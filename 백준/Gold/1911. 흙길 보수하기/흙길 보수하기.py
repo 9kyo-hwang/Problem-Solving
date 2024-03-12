d01ac0c194211cd1190e242c9f3facbdb2a91b02
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2024-03-12 21:11:11 +0900

    [Gold V] Title: 흙길 보수하기, Time: 56 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.py
+++ 백준/Gold/1911. 흙길 보수하기/흙길 보수하기.py
@@ -3,12 +3,17 @@ input = open(0).readline
 N, L = map(int, input().split())
 pools = sorted([tuple(map(int, input().split())) for _ in range(N)])
 
-prev_plank_end, ans = 0, 0
+prev_plank_end, ans = pools[0][0], 0
 for begin, end in pools:
-    real_begin = max(prev_plank_end, begin)
-    while real_begin < end:
-        real_begin += L
-        ans += 1
-    prev_plank_end = real_begin
+    if prev_plank_end > end:
+        continue
+    
+    prev_plank_end = max(prev_plank_end, begin)
+    dist = end - prev_plank_end
+    
+    plank_cnt = dist // L + (dist % L > 0)
+    
+    ans += plank_cnt
+    prev_plank_end += plank_cnt * L
 
 print(ans)
