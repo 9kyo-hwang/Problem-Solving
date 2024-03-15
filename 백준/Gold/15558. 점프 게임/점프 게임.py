7438ada3453e89885fae5c47c2e2796649b03cde
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2024-03-15 12:43:22 +0900

    [Gold V] Title: 점프 게임, Time: 232 ms, Memory: 34044 KB -BaekjoonHub

--- 백준/Gold/15558. 점프 게임/점프 게임.py
+++ 백준/Gold/15558. 점프 게임/점프 게임.py
@@ -5,8 +5,8 @@ N, k = map(int, input().split())
 lines = [list(map(int, input().rstrip())) for _ in range(2)]
 
 
-def out_of_bound(c) -> bool:
-    return not (0 <= c < N)
+def out_of_bound(tick, c) -> bool:
+    return not (tick < c < N)
 
 
 def bfs() -> bool:
@@ -15,18 +15,15 @@ def bfs() -> bool:
         for _ in range(len(q)):
             r, c = q.popleft()
             
-            lines[0][tick] = 0
-            lines[1][tick] = 0
-            
             for nr, nc in ((r, c + 1), (r, c - 1), (r ^ 1, c + k)):
                 if nc >= N:
                     return True
                 
-                if out_of_bound(nc) or lines[nr][nc] == 0:
+                if out_of_bound(tick, nc) or lines[nr][nc] == 0:
                     continue
                 
                 lines[nr][nc] = 0
                 q.append((nr, nc))
-
+    
 
 print(1 if bfs() else 0)
