6a742295d350eadd9967f4d52479c4a8ed21241d
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2024-03-15 11:58:56 +0900

    [Silver II] Title: 로또, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.py
+++ 백준/Silver/6603. 로또/로또.py
@@ -2,8 +2,7 @@ from itertools import combinations
 
 input = open(0).readline
 
-while (lst := list(map(int, input().split()))) and lst[0] != 0:
-    k, S = lst[0], lst[1:]
-    for comb in combinations(S, 6):
-        print(*comb)
+while (S := list(map(str, input().split()))) and S[0] != 0:
+    for comb in combinations(S[1:], 6):
+        print(' '.join(comb))
     print()
