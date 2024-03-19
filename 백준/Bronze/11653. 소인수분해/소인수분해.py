c8a22d42a38509e2479fd0924019bd536148c909
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2024-03-19 22:43:30 +0900

    [Bronze I] Title: 소인수분해, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -0,0 +1,15 @@
+input = open(0).readline
+
+N = int(input())
+
+while N % 2 == 0:
+    print(2)
+    N //= 2
+    
+for i in range(3, int(N**0.5) + 1, 2):
+    while N % i == 0:
+        print(i)
+        N //= i
+        
+if N > 2:
+    print(N)
