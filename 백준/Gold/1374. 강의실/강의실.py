206604e85f643e11ba8c00bffb88293411f29e07
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2024-03-26 15:45:28 +0900

    [Gold V] Title: 강의실, Time: 320 ms, Memory: 58260 KB -BaekjoonHub

--- 백준/Gold/1374. 강의실/강의실.py
+++ 백준/Gold/1374. 강의실/강의실.py
@@ -5,15 +5,11 @@ N = int(input())
 lectures = [tuple(map(int, input().split())) for _ in range(N)]
 lectures.sort(key=lambda lecture: (lecture[1], lecture[2]))
 
-room = []
-for num, s, t in lectures:
-    if not room:
-        heappush(room, t)
-        continue
-    
-    if room[0] <= s:
+room = [0]  # 강의 종료 시간 기록
+for _, begin, end in lectures:
+    if room[0] <= begin:  # 가장 빨리 끝나는 종료 시간 <= 강의 시작 시간
         heappop(room)
         
-    heappush(room, t)
+    heappush(room, end)
     
 print(len(room))
