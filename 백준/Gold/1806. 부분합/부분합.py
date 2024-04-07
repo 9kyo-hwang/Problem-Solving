df74a7d5db2efc075b805645d3bc9273f6affbd6
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2024-04-07 20:16:35 +0900

    [Gold IV] Title: 부분합, Time: 116 ms, Memory: 42204 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -1,18 +1,17 @@
-from collections import deque
 input = open(0).readline
 
 N, S = map(int, input().split())
 seq = list(map(int, input().split()))
 subtotal = 0
-q = deque([])
 
+ptr1 = 0
 ans = 100001
-for num in seq:
-    q.append(num)
+for ptr2, num in enumerate(seq):
     subtotal += num
     
-    while q and subtotal >= S:
-        ans = min(ans, len(q))
-        subtotal -= q.popleft()
+    while ptr1 <= ptr2 and subtotal >= S:
+        ans = min(ans, ptr2 - ptr1 + 1)
+        subtotal -= seq[ptr1]
+        ptr1 += 1
         
 print(0 if ans == 100001 else ans)
