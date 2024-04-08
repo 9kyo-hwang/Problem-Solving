868108187ccbd59e278a4600cb12855a20ba39c6
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2024-04-08 13:38:33 +0900

    [Gold III] Title: 웜홀, Time: 236 ms, Memory: 112252 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.py
+++ 백준/Gold/1865. 웜홀/웜홀.py
@@ -1,32 +1,34 @@
 input = open(0).readline
 INF = 10**9
 
-def bellman_ford(G: list, d: list, N: int):
+def bellman_ford(G: list, d: list, N: int) -> bool:
     for _ in range(N - 1):
-        for u, v, w in G:
-            if d[u] + w < d[v]:
-                d[v] = d[u] + w
+        for u in range(1, N + 1):
+            for v, w in G[u]:
+                if d[u] + w < d[v]:
+                    d[v] = d[u] + w
                 
     
-    for u, v, w in G:
-        if d[u] + w < d[v]:
-            return False
+    for u in range(1, N + 1):
+        for v, w in G[u]:
+            if d[u] + w < d[v]:
+                return False  # cycle detected
             
     return True
 
 
 for _ in range(TC := int(input())):
     N, M, W = map(int, input().split())
-    graph = []
+    graph = [[] for _ in range(N + 1)]
     
     for _ in range(M):
         S, E, T = map(int, input().split())
-        graph.append((S, E, T))
-        graph.append((E, S, T))
+        graph[S].append((E, T))
+        graph[E].append((S, T))
         
     for _ in range(W):
         S, E, T = map(int, input().split())
-        graph.append((S, E, -T))
+        graph[S].append((E, -T))
     
     distances = [INF] * (N + 1)
     result = bellman_ford(graph, distances, N)
