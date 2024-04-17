184adf64454257bd35ef531cc8a3ebe900f18396
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2024-04-17 13:17:25 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 600 ms, Memory: 141552 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -1,18 +1,6 @@
-import random
 input = open(0).readline
 
 N = int(input())
 arr = [int(input()) for _ in range(N)]
-
-def quicksort(arr: list) -> list:
-    if len(arr) <= 1:
-        return arr
-        
-    pivot = arr[random.randrange(0, len(arr) - 1)]
-    left = [x for x in arr if x < pivot]
-    middle = [x for x in arr if x == pivot]
-    right = [x for x in arr if x > pivot]
-    
-    return quicksort(left) + middle + quicksort(right)
-    
-print(*quicksort(arr))
+arr.sort()
+print('\n'.join(map(str, arr)))
