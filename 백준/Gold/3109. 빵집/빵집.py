2e0cc5d6fd897f6f4064663c1e0064b49fa649e4
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2024-04-20 17:12:06 +0900

    [Gold II] Title: 빵집, Time: 1184 ms, Memory: 341420 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.py
+++ 백준/Gold/3109. 빵집/빵집.py
@@ -8,11 +8,11 @@ def out_of_bound(x: int, y: int) -> bool:
     return not(0 <= x < R and 0 <= y < C)
 
 
-def dfs(x: int, y: int) -> bool:
+def dfs(x: int, y: int) -> int:
     if out_of_bound(x, y) or board[x][y] == 'x':
-        return False
+        return 0
     elif y == C - 1:
-        return True
+        return 1
     else:
         board[x][y] = 'x'
         return dfs(x - 1, y + 1) or dfs(x, y + 1) or dfs(x + 1, y + 1)
