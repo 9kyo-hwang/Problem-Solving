7495654ca580aafad8ad94ab75c65da236241128
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2024-04-25 01:02:57 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.02 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -2,13 +2,6 @@ import java.util.*;
 
 public class Solution {
     public int solution(int n) {
-        int ans = 0;
-
-        while(n > 0) {
-            ans += n & 1;
-            n >>= 1;
-        }
-
-        return ans;
+        return Integer.bitCount(n);
     }
 }
