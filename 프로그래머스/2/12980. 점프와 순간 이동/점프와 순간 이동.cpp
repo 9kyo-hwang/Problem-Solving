7917b533cdda2e73b6496451fec28825e973e3bb
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2024-04-25 01:03:59 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 3.8 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
@@ -1,12 +1,5 @@
 using namespace std;
-constexpr int MAX = 1000000000;
 
 int solution(int n) {
-    if(n == 0) {
-        return 0;
-    } else if(n < 0) {
-        return MAX;
-    } else {
-        return n & 1 ? solution(n - 1) + 1 : solution(n / 2);
-    }
+    return __builtin_popcount(n);
 }
