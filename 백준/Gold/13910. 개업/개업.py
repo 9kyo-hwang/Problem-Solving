2ede16836ec1882ddc3d9e735cb4a4a439c54a86
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2024-05-14 18:03:16 +0900

    [Gold V] Title: 개업, Time: 412 ms, Memory: 114144 KB -BaekjoonHub

--- 백준/Gold/13910. 개업/개업.py
+++ 백준/Gold/13910. 개업/개업.py
@@ -1,27 +1,24 @@
+from collections import deque
 input = open(0).readline
 
 N, M = map(int, input().split())
 S = list(map(int, input().split()))
-wok_set = set(S + [S[i] + S[j] for i in range(M - 1) for j in range(i + 1, M) if S[i] + S[j] <= N])
+S = set(S + [S[i] + S[j] for i in range(M - 1) for j in range(i + 1, M) if S[i] + S[j] <= N])
 
-dp = [-1] * (N + 1)  # 각 짜장면 개수 별 최소 요리 횟수
-for wok in wok_set:
-    dp[wok] = 1
+counts = [-1] * (N + 1)
+q = deque()
+
+for wok in S:  # start node
+    counts[wok] = 1
+    q.append(wok)
     
-for n in range(1, N + 1):
-    if dp[n] > -1:
-        continue
-    
-    min_cook_cnt = float('inf')
-    for k in range(1, n):
-        if dp[k] == -1 or dp[n - k] == -1:
+while q:
+    cook = q.popleft()
+    for wok in S:  # distance == node
+        if cook + wok > N or counts[cook + wok] > -1:
             continue
         
-        min_cook_cnt = min(min_cook_cnt, dp[k] + dp[n - k])
-        dp[n] = min(dp[n], dp[k] + dp[n - k])
-    
-    if min_cook_cnt != float('inf'):
-        dp[n] = min_cook_cnt
-
-
-print(dp[N])
+        counts[cook + wok] = counts[cook] + 1
+        q.append(cook + wok)
+        
+print(counts[N])
