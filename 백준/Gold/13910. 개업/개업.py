26e02cd7ef2958591825bf5381ff172b26534716
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2024-05-14 18:14:41 +0900

    [Gold V] Title: 개업, Time: 304 ms, Memory: 114148 KB -BaekjoonHub

--- 백준/Gold/13910. 개업/개업.py
+++ 백준/Gold/13910. 개업/개업.py
@@ -3,7 +3,7 @@ input = open(0).readline
 
 N, M = map(int, input().split())
 S = list(map(int, input().split()))
-S = set(S + [S[i] + S[j] for i in range(M - 1) for j in range(i + 1, M) if S[i] + S[j] <= N])
+S = S + [S[i] + S[j] for i in range(M - 1) for j in range(i + 1, M) if S[i] + S[j] <= N]
 
 counts = [-1] * (N + 1)
 q = deque()
