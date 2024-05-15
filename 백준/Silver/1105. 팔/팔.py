ec0aba3f8661f5a60d2d1cb9d224a7fe1118a483
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2024-05-15 15:56:25 +0900

    [Silver I] Title: 팔, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1105. 팔/팔.py
+++ 백준/Silver/1105. 팔/팔.py
@@ -1,19 +1,12 @@
-def main():
-    import sys
-    input = sys.stdin.read
-    L, R = input().split()
-    
-    if len(L) != len(R):
-        print(0)
-        return
-    
-    cnt = 0
+L, R = input().split()
+cnt = 0
+
+if len(L) == len(R):
     for l, r in zip(L, R):
-        if l == r and l == '8':
-            cnt += 1
-        elif l != r:
+        if l != r:
             break
-    print(cnt)
-    
-if __name__ == "__main__":
-    main()
+        
+        if l == '8':
+            cnt += 1
+            
+print(cnt)
