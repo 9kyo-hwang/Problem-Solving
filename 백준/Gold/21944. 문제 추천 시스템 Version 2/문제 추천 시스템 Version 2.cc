a5d89aa44154adb9bacddb67fabcac766ac13430
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2024-05-30 21:31:11 +0900

    [Gold II] Title: 문제 추천 시스템 Version 2, Time: 112 ms, Memory: 12668 KB -BaekjoonHub

--- 백준/Gold/21944. 문제 추천 시스템 Version 2/문제 추천 시스템 Version 2.cc
+++ 백준/Gold/21944. 문제 추천 시스템 Version 2/문제 추천 시스템 Version 2.cc
@@ -1,24 +1,146 @@
 #include <iostream>
 #include <set>
+#include <memory>
 
 using namespace std;
+
+class Database
+{
+public:
+    int Recommend1(int G, int x)
+    {
+        if(x == 1)
+        {
+            return Recommend1P(G);
+        }
+        else if(x == -1)
+        {
+            return Recommend1N(G);
+        }
+    }
+    
+    int Recommend2(int x)
+    {
+        if(x == 1)
+        {
+            return Recommend2P();
+        }
+        else if(x == -1)
+        {
+            return Recommend2N();
+        }
+    }
+    
+    int Recommend3(int x, int L)
+    {
+        if(x == 1)
+        {
+            return Recommend3P(L);
+        }
+        else if(x == -1)
+        {
+            return Recommend3N(L);
+        }
+    }
+
+    void Add(int P, int L, int G)
+    {
+        ProblemByLevel[L].emplace(P);
+        ProblemByLevelAndGroup[L][G].emplace(P);
+        Problems[P] = {L, G};
+    }
+    
+    void Solved(int P)
+    {
+        ProblemByLevel[Problems[P].first].erase(P);
+        ProblemByLevelAndGroup[Problems[P].first][Problems[P].second].erase(P);
+    }
+    
+private:
+    set<int> ProblemByLevel[101];
+    set<int> ProblemByLevelAndGroup[101][101];
+    pair<int, int> Problems[100001];
+    
+    int Recommend1P(int G)
+    {
+        for(int L = 100; L >= 1; --L)
+        {
+            if(!ProblemByLevelAndGroup[L][G].empty())
+            {
+                return *(--ProblemByLevelAndGroup[L][G].end());
+            }
+        }
+    }
+    
+    int Recommend1N(int G)
+    {
+        for(int L = 1; L <= 100; ++L)
+        {
+            if(!ProblemByLevelAndGroup[L][G].empty())
+            {
+                return *ProblemByLevelAndGroup[L][G].begin();
+            }
+        }
+    }
+    
+    int Recommend2P()
+    {
+        for(int L = 100; L >= 1; --L)
+        {
+            if(!ProblemByLevel[L].empty())
+            {
+                return *(--ProblemByLevel[L].end());
+            }
+        }
+    }
+    
+    int Recommend2N()
+    {
+        for(int L = 1; L <= 100; ++L)
+        {
+            if(!ProblemByLevel[L].empty())
+            {
+                return *ProblemByLevel[L].begin();
+            }
+        }
+    }
+    
+    int Recommend3P(int L)
+    {
+        for(; L <= 100; ++L)
+        {
+            if(!ProblemByLevel[L].empty())
+            {
+                return *ProblemByLevel[L].begin();
+            }
+        }
+        return -1;
+    }
+    
+    int Recommend3N(int L)
+    {
+        for(L -= 1; L >= 1; --L)
+        {
+            if(!ProblemByLevel[L].empty())
+            {
+                return *(--ProblemByLevel[L].end());
+            }
+        }
+        return -1;
+    }
+};
 
 int main()
 {
     cin.tie(nullptr)->sync_with_stdio(false);
     
-    set<int> ProblemByLevel[101];
-    set<int> ProblemByLevelAndGroup[101][101];
-    pair<int, int> Problems[100001];
+    auto DB = make_unique<Database>();
     
     int N; cin >> N;
     while(N--)
     {
         int P, L, G; cin >> P >> L >> G;
-        
-        ProblemByLevel[L].emplace(P);
-        ProblemByLevelAndGroup[L][G].emplace(P);
-        Problems[P] = {L, G};
+        DB->Add(P, L, G);
     }
     
     int M; cin >> M;
@@ -28,102 +150,27 @@ int main()
         if(Command == "recommend")  // find Max/Min in Category G
         {
             int G, x; cin >> G >> x;
-            if(x == 1)
-            {
-                for(int L = 100; L >= 1; --L)
-                {
-                    if(!ProblemByLevelAndGroup[L][G].empty())
-                    {
-                        cout << *(--ProblemByLevelAndGroup[L][G].end()) << "\n";
-                        break;
-                    }
-                }
-            }
-            else if(x == -1)
-            {
-                for(int L = 1; L <= 100; ++L)
-                {
-                    if(!ProblemByLevelAndGroup[L][G].empty())
-                    {
-                        cout << *ProblemByLevelAndGroup[L][G].begin() << "\n";
-                        break;
-                    }
-                }
-            }
+            cout << DB->Recommend1(G, x) << "\n";
         }
         else if(Command == "recommend2")  // find Max/Min any Category
         {
             int x; cin >> x;
-            if(x == 1)
-            {
-                for(int L = 100; L >= 1; --L)
-                {
-                    if(!ProblemByLevel[L].empty())
-                    {
-                        cout << *(--ProblemByLevel[L].end()) << "\n";
-                        break;
-                    }
-                }
-            }
-            else if(x == -1)
-            {
-                for(int L = 1; L <= 100; ++L)
-                {
-                    if(!ProblemByLevel[L].empty())
-                    {
-                        cout << *ProblemByLevel[L].begin() << "\n";
-                        break;
-                    }
-                }
-            }
+            cout << DB->Recommend2(x) << "\n";
         }
         else if(Command == "recommend3")  // find lower bound
         {
             int x, L; cin >> x >> L;
-            bool Flag = false;
-            if(x == 1)
-            {
-                for(; L <= 100; ++L)
-                {
-                    if(!ProblemByLevel[L].empty())
-                    {
-                        cout << *ProblemByLevel[L].begin() << "\n";
-                        Flag = true;
-                        break;
-                    }
-                }
-            }
-            else if(x == -1)
-            {
-                for(L -= 1; L >= 1; --L)
-                {
-                    if(!ProblemByLevel[L].empty())
-                    {
-                        cout << *(--ProblemByLevel[L].end()) << "\n";
-                        Flag = true;
-                        break;
-                    }
-                }
-            }
-            
-            if(!Flag)
-            {
-                cout << "-1\n";
-            }
+            cout << DB->Recommend3(x, L) << "\n";
         }
         else if(Command == "add")
         {
             int P, L, G; cin >> P >> L >> G;
-            
-            ProblemByLevel[L].emplace(P);
-            ProblemByLevelAndGroup[L][G].emplace(P);
-            Problems[P] = {L, G};
+            DB->Add(P, L, G);
         }
         else if(Command == "solved")
         {
             int P; cin >> P;
-            ProblemByLevel[Problems[P].first].erase(P);
-            ProblemByLevelAndGroup[Problems[P].first][Problems[P].second].erase(P);
+            DB->Solved(P);
         }
     }
     
