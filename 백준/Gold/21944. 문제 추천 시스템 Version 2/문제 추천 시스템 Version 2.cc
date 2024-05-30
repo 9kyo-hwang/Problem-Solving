a19afc2002c70967b2786d173f25a537ae37aa1d
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2024-05-30 21:16:38 +0900

    [Gold II] Title: 문제 추천 시스템 Version 2, Time: 124 ms, Memory: 12544 KB -BaekjoonHub

--- 백준/Gold/21944. 문제 추천 시스템 Version 2/문제 추천 시스템 Version 2.cc
+++ 백준/Gold/21944. 문제 추천 시스템 Version 2/문제 추천 시스템 Version 2.cc
@@ -1,25 +1,23 @@
 #include <iostream>
 #include <set>
-#include <map>
 
 using namespace std;
 
 int main()
 {
     cin.tie(nullptr)->sync_with_stdio(false);
-
-    using FProblem = pair<int, int>;  // L, P
-
-    map<int, multiset<FProblem>> Groups;  // G : {L, P}, ...
-    multiset<FProblem> Levels;              // {L, P}, ...
-    map<int, FProblem> Problems;                // P : {L, G}
+    
+    set<int> ProblemByLevel[101];
+    set<int> ProblemByLevelAndGroup[101][101];
+    pair<int, int> Problems[100001];
     
     int N; cin >> N;
     while(N--)
     {
         int P, L, G; cin >> P >> L >> G;
-        Groups[G].emplace(L, P);
-        Levels.emplace(L, P);
+        
+        ProblemByLevel[L].emplace(P);
+        ProblemByLevelAndGroup[L][G].emplace(P);
         Problems[P] = {L, G};
     }
     
@@ -32,11 +30,25 @@ int main()
             int G, x; cin >> G >> x;
             if(x == 1)
             {
-                cout << (--Groups[G].end())->second << "\n";
+                for(int L = 100; L >= 1; --L)
+                {
+                    if(!ProblemByLevelAndGroup[L][G].empty())
+                    {
+                        cout << *(--ProblemByLevelAndGroup[L][G].end()) << "\n";
+                        break;
+                    }
+                }
             }
             else if(x == -1)
             {
-                cout << Groups[G].begin()->second << "\n";
+                for(int L = 1; L <= 100; ++L)
+                {
+                    if(!ProblemByLevelAndGroup[L][G].empty())
+                    {
+                        cout << *ProblemByLevelAndGroup[L][G].begin() << "\n";
+                        break;
+                    }
+                }
             }
         }
         else if(Command == "recommend2")  // find Max/Min any Category
@@ -44,57 +56,74 @@ int main()
             int x; cin >> x;
             if(x == 1)
             {
-                cout << (--Levels.end())->second << "\n";
+                for(int L = 100; L >= 1; --L)
+                {
+                    if(!ProblemByLevel[L].empty())
+                    {
+                        cout << *(--ProblemByLevel[L].end()) << "\n";
+                        break;
+                    }
+                }
             }
             else if(x == -1)
             {
-                cout << Levels.begin()->second << "\n";
+                for(int L = 1; L <= 100; ++L)
+                {
+                    if(!ProblemByLevel[L].empty())
+                    {
+                        cout << *ProblemByLevel[L].begin() << "\n";
+                        break;
+                    }
+                }
             }
         }
         else if(Command == "recommend3")  // find lower bound
         {
             int x, L; cin >> x >> L;
+            bool Flag = false;
             if(x == 1)
             {
-                auto it = Levels.lower_bound({L, 0});
-                if(it != Levels.end())
+                for(; L <= 100; ++L)
                 {
-                    cout << it->second << "\n";
-                }
-                else
-                {
-                    cout << "-1\n";
+                    if(!ProblemByLevel[L].empty())
+                    {
+                        cout << *ProblemByLevel[L].begin() << "\n";
+                        Flag = true;
+                        break;
+                    }
                 }
             }
             else if(x == -1)
             {
-                auto it = Levels.upper_bound({L, 0});
-                if(it != Levels.begin())
+                for(L -= 1; L >= 1; --L)
                 {
-                    cout << (--it)->second << "\n";
+                    if(!ProblemByLevel[L].empty())
+                    {
+                        cout << *(--ProblemByLevel[L].end()) << "\n";
+                        Flag = true;
+                        break;
+                    }
                 }
-                else
-                {
-                    cout << "-1\n";
-                }
+            }
+            
+            if(!Flag)
+            {
+                cout << "-1\n";
             }
         }
         else if(Command == "add")
         {
             int P, L, G; cin >> P >> L >> G;
             
-            Groups[G].emplace(L, P);
-            Levels.emplace(L, P);
+            ProblemByLevel[L].emplace(P);
+            ProblemByLevelAndGroup[L][G].emplace(P);
             Problems[P] = {L, G};
         }
         else if(Command == "solved")
         {
             int P; cin >> P;
-            const auto& [L, G] = Problems[P];
-            
-            Groups[G].erase({L, P});
-            Levels.erase({L, P});
-            Problems.erase(P);
+            ProblemByLevel[Problems[P].first].erase(P);
+            ProblemByLevelAndGroup[Problems[P].first][Problems[P].second].erase(P);
         }
     }
     
