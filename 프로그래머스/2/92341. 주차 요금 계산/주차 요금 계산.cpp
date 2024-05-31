684a20977f08b4f13069fee99b63984805ed129d
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2024-05-31 18:08:18 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <map>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+tuple<int, string, string> RecordParser(const string& InRecord)
+{
+    istringstream SS(InRecord);
+    string Buffer;
+    
+    // 1. Time Parsing
+    SS >> Buffer;
+    int Pos = Buffer.find(':');
+    int Hour = stoi(Buffer.substr(0, Pos));
+    int Minute = stoi(Buffer.substr(Pos + 1));
+    int Time = Hour * 60 + Minute;
+    
+    // 2. License Parsing
+    SS >> Buffer;
+    string License = Buffer;
+    
+    // 3. History Parsing
+    SS >> Buffer;
+    string History = Buffer;
+    
+    return {Time, License, History};
+}
+
+constexpr int LAST_EXIT = 1439;  // 23 * 60 + 59
+
+tuple<int, int, int, int> FeeParser(const vector<int>& InFees)
+{
+    return {InFees[0], InFees[1], InFees[2], InFees[3]};
+}
+
+vector<int> solution(vector<int> InFees, vector<string> InRecords) 
+{
+    unordered_map<string, int> EntryMap, ParkingMap;
+    for(const string& Record : InRecords)
+    {
+        const auto& [Time, License, History] = RecordParser(Record);
+        if(History == "IN")
+        {
+            EntryMap[License] = Time;
+            continue;
+        }
+        
+        int EntryTime = EntryMap[License];
+        EntryMap.erase(License);
+        
+        ParkingMap[License] += Time - EntryTime;
+    }
+    
+    for(const auto& [License, EntryTime] : EntryMap)
+    {
+        ParkingMap[License] += LAST_EXIT - EntryTime;
+    }
+    
+    const auto& [DefaultTime, DefaultFee, UnitTime, UnitFee] = FeeParser(InFees);
+    map<string, int> FeeMap;
+    
+    for(auto& [License, Time] : ParkingMap)
+    {
+        if(FeeMap.find(License) == FeeMap.end())
+        {
+            FeeMap[License] = DefaultFee;
+            Time = clamp(Time - DefaultTime, 0, Time);
+        }
+        
+        FeeMap[License] += ceil((double)Time / (double)UnitTime) * UnitFee;
+    }
+
+    vector<int> Answer;
+    for(const auto& [License, Fee] : FeeMap)
+    {
+        Answer.emplace_back(Fee);
+    }
+    return Answer;
+}
