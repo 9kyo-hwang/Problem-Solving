962c5ebd363addfc6f2eae5495c23299956c0cc6
구교황 <49135176+9kyo-hwang@users.noreply.github.com>
2024-06-04 17:57:57 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 4.39 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.cpp
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.cpp
@@ -5,17 +5,15 @@ using namespace std;
 
 vector<string> solution(int n, vector<int> Arr1, vector<int> Arr2) 
 {
-    vector<string> Answer;
+    vector<string> Answer(n, "");
     for(int i = 0; i < n; ++i)
     {
         int x = Arr1[i] | Arr2[i];
-        string Decryption{};
         for(int j = 0; j < n; ++j)
         {
-            Decryption += (x & 1 ? '#' : ' ');
+            Answer[i].insert(0, (x & 1 ? "#" : " "));
             x >>= 1;
         }
-        Answer.emplace_back(string(Decryption.rbegin(), Decryption.rend()));
     }
     return Answer;
 }
